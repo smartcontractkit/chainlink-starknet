9c2a6278a272b631b5e92b06563ca4f1d1ec66f2
Blaž Hrastnik <blaz@mxxn.io>
2022-04-22 09:54:35 +0900

    Initial import with Nix environment & shell

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "StarkNet integration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system: 
+      let
+        pkgs = import nixpkgs { inherit system; overlays = [ ]; };
+      in rec {
+        devShell = pkgs.callPackage ./shell.nix {};
+      });
+}
