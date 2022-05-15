cb5853ab5ee7fe16153f11475eda94f585a12920
Sergei Drugalev <sergei.drugalev@smartcontract.com>
2022-05-15 22:35:56 +0200

    Initial relay structure

---
+++ pkg/starknet/contract/config_digester.go
@@ -0,0 +1,21 @@
+package contract
+
+import (
+	"github.com/smartcontractkit/libocr/offchainreporting2/types"
+)
+
+var _ types.OffchainConfigDigester = (*OffchainConfigDigester)(nil)
+
+type OffchainConfigDigester struct {
+	// todo: add params
+}
+
+func (d OffchainConfigDigester) ConfigDigest(cfg types.ContractConfig) (types.ConfigDigest, error) {
+	// todo: implement
+	return types.ConfigDigest{}, nil
+}
+
+func (OffchainConfigDigester) ConfigDigestPrefix() types.ConfigDigestPrefix {
+	// todo: implement
+	return 0
+}
---
+++ pkg/starknet/types.go
@@ -0,0 +1,7 @@
+package starknet
+
+// CL Core OCR2 job spec RelayConfig member
+type RelayConfig struct {
+	// todo: add params
+	ChainID string `json:"chainID"`
+}
