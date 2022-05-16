a4018820bee7d09ee2d3278411c1c3d5e990951e
Sergei Drugalev <sergei.drugalev@smartcontract.com>
2022-05-17 00:34:54 +0200

    Interface for ChainCfg

--- pkg/starknet/db/db.go
+++ pkg/starknet/db/db.go
@@ -4,7 +4,11 @@ import (
 	"database/sql/driver"
 	"encoding/json"
 	"errors"
+	// "github.com/smartcontractkit/chainlink/core/chains"
 )
+
+// todo: uncomment when interface released
+// var _ chains.Config = (*ChainCfg)(nil)
 
 type ChainCfg struct {
 	// chain configuration values in db
@@ -13,7 +17,7 @@ type ChainCfg struct {
 func (c *ChainCfg) Scan(value interface{}) error {
 	b, ok := value.([]byte)
 	if !ok {
-		return errors.New("config is not []byte")
+		return errors.New("type assertion to []byte failed")
 	}
 
 	return json.Unmarshal(b, c)
