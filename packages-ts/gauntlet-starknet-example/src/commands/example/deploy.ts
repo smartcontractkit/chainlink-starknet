c507f538dcc99955ffa7d7732af4f8a2739c9d1b
Akhil Chainani <akhil.chainani@smartcontract.com>
2022-05-18 10:47:35 -0400

    use example structs from blueprint in gauntlet starknet

--- packages-ts/gauntlet-starknet-example/src/commands/example/deploy.ts
+++ packages-ts/gauntlet-starknet-example/src/commands/example/deploy.ts
@@ -7,51 +7,39 @@ import {
 } from '@chainlink/gauntlet-starknet'
 import { CATEGORIES } from '@chainlink/gauntlet-core'
 import { tokenContractLoader } from '../../lib/contracts'
-
-type UserInput = {
-  address: string
-}
+import { DeployExample, DeployExampleInput } from '@chainlink/gauntlet-contracts-example'
 
 type ContractInput = {}
 
-const makeUserInput = async (flags, args): Promise<UserInput> => {
-  if (flags.input) return flags.input as UserInput
+const makeUserInput = async (flags, args): Promise<DeployExampleInput> => {
+  if (flags.input) return flags.input as DeployExampleInput
   return {
     address: flags.address,
   }
 }
 
-const makeContractInput = async (input: UserInput): Promise<ContractInput> => {
+const makeContractInput = async (input: DeployExampleInput): Promise<ContractInput> => {
   return {}
 }
 
-const validate: Validation<UserInput> = async (input) => {
+const validate: Validation<DeployExampleInput> = async (input) => {
   return true
 }
 
 // This is a custom beforeExecute hook executed right before the command action is executed
-const beforeExecute: BeforeExecute<UserInput, ContractInput> = (context, input, deps) => async () => {
+const beforeExecute: BeforeExecute<DeployExampleInput, ContractInput> = (context, input, deps) => async () => {
   deps.logger.info('About to deploy a Sample Contract')
   await deps.prompt('Continue?')
 }
 // This is a custom afterExecute hook executed right after the command action is executed
-const afterExecute: AfterExecute<UserInput, ContractInput> = (context, input, deps) => async (result) => {
+const afterExecute: AfterExecute<DeployExampleInput, ContractInput> = (context, input, deps) => async (result) => {
   deps.logger.info(
     `Contract deployed with address: ${result.responses[0].tx.address} at tx hash: ${result.responses[0].tx.hash}`,
   )
 }
 
-const commandConfig: ExecuteCommandConfig<UserInput, ContractInput> = {
-  contractId: 'exmaple',
-  category: CATEGORIES.EXAMPLE,
-  action: 'deploy',
-  ux: {
-    description: 'A sample contract deployment command',
-    examples: [`${CATEGORIES.EXAMPLE}:deploy --network=<NETWORK> --address=<ADDRESS> <CONTRACT_ADDRESS>`],
-  },
-  makeUserInput,
-  makeContractInput,
-  validations: [validate],
+const commandConfig: ExecuteCommandConfig<DeployExampleInput, ContractInput> = {
+  ...DeployExample,
   loadContract: tokenContractLoader,
   hooks: {
     beforeExecute,
--- packages-ts/gauntlet-starknet-example/src/commands/example/increaseBalance.ts
+++ packages-ts/gauntlet-starknet-example/src/commands/example/increaseBalance.ts
@@ -1,39 +1,24 @@
 import { ExecuteCommandConfig, makeExecuteCommand, Validation } from '@chainlink/gauntlet-starknet'
 import { CATEGORIES } from '@chainlink/gauntlet-core'
 import { tokenContractLoader } from '../../lib/contracts'
-
-type UserInput = {
-  balance: number
-}
+import { IncreaseBalance, IncreaseBalanceInput } from '@chainlink/gauntlet-contracts-example'
 
 type ContractInput = [number]
 
-const makeUserInput = async (flags, args): Promise<UserInput> => {
-  if (flags.input) return flags.input as UserInput
+const makeUserInput = async (flags, args): Promise<IncreaseBalanceInput> => {
+  if (flags.input) return flags.input as IncreaseBalanceInput
   return {
     balance: flags.balance,
   }
 }
 
-const makeContractInput = async (input: UserInput): Promise<ContractInput> => {
+const makeContractInput = async (input: IncreaseBalanceInput): Promise<ContractInput> => {
   return [Number(input.balance)]
 }
 
-const validate: Validation<UserInput> = async (input) => {
-  return true
-}
-
-const commandConfig: ExecuteCommandConfig<UserInput, ContractInput> = {
-  contractId: 'exmaple',
-  category: CATEGORIES.EXAMPLE,
-  action: 'increase_balance',
-  ux: {
-    description: 'A simple exmaple contract call - in this case increase_balance',
-    examples: ['token:deploy --network=<NETWORK> --address=<ADDRESS> <CONTRACT_ADDRESS>'],
-  },
-  makeUserInput,
-  makeContractInput,
-  validations: [validate],
+const commandConfig: ExecuteCommandConfig<IncreaseBalanceInput, ContractInput> = {
+  ...IncreaseBalance,
+  makeContractInput: makeContractInput,
   loadContract: tokenContractLoader,
 }
 
