9e0b91a75b54b080cf6e8c965a6483884c573206
RodrigoAD <15104916+RodrigoAD@users.noreply.github.com>
2022-05-24 14:40:02 +0200

    start oz erc20 package

--- packages-ts/gauntlet-starknet-cli/src/index.ts
+++ packages-ts/gauntlet-starknet-cli/src/index.ts
@@ -1,13 +1,15 @@
 import OCR2Commands from '@chainlink/gauntlet-starknet-ocr2'
 import ExampleCommands from '@chainlink/gauntlet-starknet-example'
 import AccountCommands from '@chainlink/gauntlet-starknet-account'
+import ERC20Commands from '@chainlink/gauntlet-starknet-oz-erc20'
+
 import { executeCLI } from '@chainlink/gauntlet-core'
 import { existsSync } from 'fs'
 import path from 'path'
 import { io } from '@chainlink/gauntlet-core/dist/utils'
 
 const commands = {
-  custom: [...OCR2Commands, ...ExampleCommands, ...AccountCommands],
+  custom: [...OCR2Commands, ...ExampleCommands, ...AccountCommands, ...ERC20Commands],
   loadDefaultFlags: () => ({}),
   abstract: {
     findPolymorphic: () => undefined,
