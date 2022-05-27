74c27c383db9fb856514f753871455fce5883601
Akhil Chainani <akhil.chainani@smartcontract.com>
2022-05-27 16:32:34 -0400

    set_config improvements

--- packages-ts/gauntlet-starknet-ocr2/src/commands/aggregator/setConfig.ts
+++ packages-ts/gauntlet-starknet-ocr2/src/commands/aggregator/setConfig.ts
@@ -8,6 +8,7 @@ import {
 import { BN } from '@chainlink/gauntlet-core/dist/utils'
 import { CATEGORIES } from '../../lib/categories'
 import { ocr2ContractLoader } from '../../lib/contracts'
+import { ec, number } from 'starknet'
 
 type OnchainConfig = any // TODO: Define more clearly
 type OffchainConfig = any // TODO: Define more clearly
@@ -23,17 +24,18 @@ type UserInput = {
   transmitters: string[]
   onchainConfig: OnchainConfig
   offchainConfig: OffchainConfig
+  offchainConfigVersion: number
 }
 
-type ContractInput = {
-  oracles: Oracle[]
-  oracles_len: number
-  f: number
-  onchain_config: string
-  offchain_config_version: number
-  offchain_config_len: number
-  offchain_config: string
-}
+type ContractInput = [
+  {
+    oracles: Oracle[]
+    f: number
+    onchain_config: string
+    offchain_config_version: number
+    offchain_config: string
+  },
+]
 
 const makeUserInput = async (flags, args): Promise<UserInput> => {
   if (flags.input) return flags.input as UserInput
@@ -42,22 +44,26 @@ const makeUserInput = async (flags, args): Promise<UserInput> => {
     f: flags.f,
     signers: flags.signers,
     transmitters: flags.transmitters,
-    onchainConfig: '',
-    offchainConfig: '',
+    onchainConfig: flags.onchainConfig || 1,
+    offchainConfig: flags.offchainConfig || [1],
+    offchainConfigVersion: flags.offchainConfigVersion || 2,
   }
 }
 
 const makeContractInput = async (input: UserInput): Promise<ContractInput> => {
-  const oracles: Oracle[] = input.signers.map((o, i) => ({ signer: o, transmitter: input.transmitters[i] }))
-  return {
-    oracles: oracles,
-    oracles_len: oracles.length,
-    f: new BN(input.f).toNumber(),
-    onchain_config: input.onchainConfig.toString('base64'),
-    offchain_config: input.offchainConfig.toString('base64'),
-    offchain_config_version: 2,
-    offchain_config_len: 1,
-  }
+  const oracles: Oracle[] = input.signers.map((o, i) => ({
+    signer: number.toBN(ec.getStarkKey(o)),
+    transmitter: input.transmitters[i],
+  }))
+  return [
+    {
+      oracles: oracles,
+      f: new BN(input.f).toNumber(),
+      onchain_config: input.onchainConfig.toString('base64'),
+      offchain_config_version: 2,
+      offchain_config: input.offchainConfig.toString('base64'),
+    },
+  ]
 }
 
 const validateInput = async (input: UserInput): Promise<boolean> => {
