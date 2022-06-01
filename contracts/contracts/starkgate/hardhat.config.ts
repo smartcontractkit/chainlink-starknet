1ae0596349f7299cd0e5431a89a39bda72b32988
qperrot <quentinperrotm@gmail.com>
2022-06-01 11:00:20 +0200

    npm starkgate and test

---
+++ contracts/contracts/starkgate/hardhat.config.ts
@@ -0,0 +1,29 @@
+import "@shardlabs/starknet-hardhat-plugin";
+
+import { HardhatUserConfig} from "hardhat/types";
+
+const config: HardhatUserConfig = {
+  solidity: '0.6.12',
+  starknet: {
+    venv: "active",
+    wallets: {
+      OpenZeppelin: {
+        accountName: "OpenZeppelin",
+        modulePath: "starkware.starknet.wallets.open_zeppelin.OpenZeppelinAccount",
+        accountPath: "~/.starknet_accounts"
+      }
+    }
+  },
+  paths: {
+    // starknetSources: __dirname + "/src/starkware/starknet/apps/starkgate/cairo",
+    // cairoPaths: ["../"]
+    starknetArtifacts: "node_modules/@chainlink-dev/starkgate-contracts/artifacts"
+  },
+  networks: {
+    devnet: {
+      url: "http://127.0.0.1:5050"
+    },
+  }
+};
+
+export default config;
