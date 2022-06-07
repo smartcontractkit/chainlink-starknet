f8bf6aaf585f893eca87d8118beb2640f005dd8e
Sergei Drugalev <sergei.drugalev@smartcontract.com>
2022-06-07 08:49:12 +0200

    OCR2 types builders

--- pkg/starknet/ocr2/types.go
+++ pkg/starknet/ocr2/types.go
@@ -2,6 +2,7 @@ package ocr2
 
 import (
 	"context"
+	"errors"
 	"math/big"
 	"time"
 
@@ -13,9 +14,31 @@ type ContractConfigDetails struct {
 	Digest types.ConfigDigest
 }
 
+func NewContractConfigDetails(blockFelt string, digestFelt string) (ccd ContractConfigDetails, err error) {
+	block, ok := new(big.Int).SetString(blockFelt, 0)
+	if !ok {
+		return ccd, errors.New("wrong format of block")
+	}
+
+	digest, ok := new(big.Int).SetString(digestFelt, 0)
+	if !ok {
+		return ccd, errors.New("wrong format of digest")
+	}
+
+	digestBytes, err := types.BytesToConfigDigest(digest.Bytes())
+	if err != nil {
+		return
+	}
+
+	return ContractConfigDetails{
+		Block:  block.Uint64(),
+		Digest: digestBytes,
+	}, nil
+}
+
 type ContractConfig struct {
-	config      types.ContractConfig
-	configBlock uint64
+	Config      types.ContractConfig
+	ConfigBlock uint64
 }
 
 type TransmissionDetails struct {
@@ -26,10 +49,32 @@ type TransmissionDetails struct {
 	latestTimestamp time.Time
 }
 
-type Reader interface {
-	OCR2ReadLatestConfigDetails(context.Context, string) (ContractConfigDetails, error)
+type BillingDetails struct {
+	observationPaymentGJuels  uint64
+	transmissionPaymentGJuels uint64
 }
 
-type Config interface {
-	// todo: add ocr2 specific config read func
+func NewBillingDetails(observationPaymentFelt string, transmissionPaymentFelt string) (bd BillingDetails, err error) {
+	observationPaymentGJuels, ok := new(big.Int).SetString(observationPaymentFelt, 0)
+	if !ok {
+		return bd, errors.New("wrong format of observationPaymentGJuels")
+	}
+
+	transmissionPaymentGJuels, ok := new(big.Int).SetString(transmissionPaymentFelt, 0)
+	if !ok {
+		return bd, errors.New("wrong format of transmissionPaymentGJuels")
+	}
+
+	return BillingDetails{
+		observationPaymentGJuels:  observationPaymentGJuels.Uint64(),
+		transmissionPaymentGJuels: transmissionPaymentGJuels.Uint64(),
+	}, nil
+}
+
+type Reader interface {
+	OCR2LatestConfigDetails(context.Context, string) (ContractConfigDetails, error)
+	OCR2LatestConfig(context.Context, string, uint64) (ContractConfig, error)
+	OCR2BillingDetails(context.Context, string) (BillingDetails, error)
+
+	LatestBlockHeight(context.Context) (uint64, error)
 }
