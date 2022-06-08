f524f043568c00b1b6cc7cbc9e5d4f5b16c5cfa3
Kyle Martin <kylesmartin19@gmail.com>
2022-06-08 16:12:14 -0400

    Add set config test with input, add public key check to oz test

--- packages-ts/gauntlet-starknet-oz/test/commands/account.test.ts
+++ packages-ts/gauntlet-starknet-oz/test/commands/account.test.ts
@@ -1,9 +1,14 @@
-import { startNetwork, IntegratedDevnet } from '@chainlink/gauntlet-starknet'
+import { startNetwork, IntegratedDevnet, makeProvider } from '@chainlink/gauntlet-starknet'
 import deployCommand from '../../src/commands/account/deploy'
-import { registerExecuteCommand, TIMEOUT } from '@chainlink/gauntlet-starknet-example/test/utils'
+import { registerExecuteCommand, TIMEOUT, LOCAL_URL } from '@chainlink/gauntlet-starknet-example/test/utils'
+import { loadContract, CONTRACT_LIST } from '../../src/lib/contracts'
+import { Contract } from 'starknet'
+import { BN } from '@chainlink/gauntlet-core/dist/utils'
 
 describe('OZ Account Contract', () => {
   let network: IntegratedDevnet
+  let publicKey: string
+  let contractAddress: string
 
   beforeAll(async () => {
     network = await startNetwork()
@@ -16,6 +21,15 @@ describe('OZ Account Contract', () => {
 
       const report = await command.execute()
       expect(report.responses[0].tx.status).toEqual('ACCEPTED')
+
+      contractAddress = report.responses[0].contract
+      publicKey = report.data.publicKey
+
+      const oz = loadContract(CONTRACT_LIST.ACCOUNT)
+      const ozContract = new Contract(oz.abi, contractAddress, makeProvider(LOCAL_URL).provider)
+      const response = await ozContract.get_public_key()
+      const onChainPubKey = response[0]
+      expect(onChainPubKey).toEqual(new BN(publicKey.split('x')[1], 16))
     },
     TIMEOUT,
   )
