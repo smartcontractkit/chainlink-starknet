c6bf0f1b0b97ee12291aca299aef62d1954f5cf4
Sergei Drugalev <sergei.drugalev@smartcontract.com>
2022-06-14 00:22:21 +0200

    Checking for stake config + config values from db

--- pkg/relay/starknet/config.go
+++ pkg/relay/starknet/config.go
@@ -2,43 +2,68 @@ package starknet
 
 import (
 	"sync"
+	"time"
 
 	"github.com/smartcontractkit/chainlink-relay/pkg/logger"
 	"github.com/smartcontractkit/chainlink-starknet/pkg/relay/starknet/db"
 )
 
 var DefaultConfigSet = ConfigSet{
-	// defaults
+	OCR2CachePollPeriod: 5 * time.Second,
+	OCR2CacheTTL:        time.Minute,
 }
 
 type ConfigSet struct {
-	// chain configuration parameters
+	OCR2CachePollPeriod time.Duration
+	OCR2CacheTTL        time.Duration
 }
 
 type Config interface {
-	// interface to interact with chain configuration
+	OCR2CachePollPeriod() time.Duration
+	OCR2CacheTTL() time.Duration
+
 	Update(db.ChainCfg)
 }
 
 var _ Config = (*config)(nil)
 
 type config struct {
-	defaults ConfigSet
-	chain    db.ChainCfg
-	chainMu  sync.RWMutex
-	lggr     logger.Logger
+	defaults  ConfigSet
+	dbCfg     db.ChainCfg
+	dbCfgLock sync.RWMutex
+	lggr      logger.Logger
 }
 
 func NewConfig(dbCfg db.ChainCfg, lggr logger.Logger) *config {
 	return &config{
 		defaults: DefaultConfigSet,
-		chain:    dbCfg,
+		dbCfg:    dbCfg,
 		lggr:     lggr,
 	}
 }
 
 func (c *config) Update(dbCfg db.ChainCfg) {
-	c.chainMu.Lock()
-	c.chain = dbCfg
-	c.chainMu.Unlock()
+	c.dbCfgLock.Lock()
+	c.dbCfg = dbCfg
+	c.dbCfgLock.Unlock()
+}
+
+func (c *config) OCR2CachePollPeriod() time.Duration {
+	c.dbCfgLock.RLock()
+	ch := c.dbCfg.OCR2CachePollPeriod
+	c.dbCfgLock.RUnlock()
+	if ch != nil {
+		return ch.Duration()
+	}
+	return c.defaults.OCR2CachePollPeriod
+}
+
+func (c *config) OCR2CacheTTL() time.Duration {
+	c.dbCfgLock.RLock()
+	ch := c.dbCfg.OCR2CacheTTL
+	c.dbCfgLock.RUnlock()
+	if ch != nil {
+		return ch.Duration()
+	}
+	return c.defaults.OCR2CacheTTL
 }
