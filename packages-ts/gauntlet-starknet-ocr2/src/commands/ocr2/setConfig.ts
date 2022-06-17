d5fc218852addcc47a2e45931317f8157fbbd6a8
RodrigoAD <15104916+RodrigoAD@users.noreply.github.com>
2022-06-17 11:57:09 +0200

    update config with latest core version

--- packages-ts/gauntlet-starknet-ocr2/src/commands/ocr2/setConfig.ts
+++ packages-ts/gauntlet-starknet-ocr2/src/commands/ocr2/setConfig.ts
@@ -1,8 +1,9 @@
-import { ExecuteCommandConfig, makeExecuteCommand } from '@chainlink/gauntlet-starknet'
+import { AfterExecute, ExecuteCommandConfig, makeExecuteCommand, BeforeExecute } from '@chainlink/gauntlet-starknet'
 import { BN } from '@chainlink/gauntlet-core/dist/utils'
 import { ocr2ContractLoader } from '../../lib/contracts'
 import { SetConfig, encoding, SetConfigInput } from '@chainlink/gauntlet-contracts-ocr2'
-import { bytesToFelts } from '../../lib/encoding'
+import { bytesToFelts, decodeOffchainConfigFromEventData } from '../../lib/encoding'
+import assert from 'assert'
 
 type Oracle = {
   signer: string
@@ -23,14 +24,32 @@ const makeContractInput = async (input: SetConfigInput): Promise<ContractInput> 
     transmitter: input.transmitters[i],
   }))
   const { offchainConfig } = await encoding.serializeOffchainConfig(input.offchainConfig, input.secret)
-  console.log('CONFIG', bytesToFelts(offchainConfig))
   return [oracles, new BN(input.f).toNumber(), input.onchainConfig, 2, bytesToFelts(offchainConfig)]
+}
+
+const afterExecute: AfterExecute<SetConfigInput, ContractInput> = (context, input, deps) => async (result) => {
+  const txHash = result.responses[0].tx.hash
+  const txInfo = await context.provider.provider.getTransactionReceipt(txHash)
+  const eventData = (txInfo.events[0] as any).data
+  const offchainConfig = decodeOffchainConfigFromEventData(eventData)
+  try {
+    assert.deepStrictEqual(offchainConfig, input.user.offchainConfig)
+    deps.logger.success('Configuration was successfully set')
+    return { successfulConfiguration: true }
+  } catch (e) {
+    deps.logger.error('Configuration set is different than provided')
+    deps.logger.log(offchainConfig)
+    return { successfulConfiguration: false }
+  }
 }
 
 const commandConfig: ExecuteCommandConfig<SetConfigInput, ContractInput> = {
   ...SetConfig,
   makeContractInput: makeContractInput,
   loadContract: ocr2ContractLoader,
+  hooks: {
+    afterExecute,
+  },
 }
 
 export default makeExecuteCommand(commandConfig)
--- packages-ts/gauntlet-starknet-ocr2/src/lib/encoding.ts
+++ packages-ts/gauntlet-starknet-ocr2/src/lib/encoding.ts
@@ -1,5 +1,6 @@
 import { number } from 'starknet'
 import { BN } from '@chainlink/gauntlet-core/dist/utils'
+import { encoding } from '@chainlink/gauntlet-contracts-ocr2'
 
 const CHUNK_SIZE = 31
 
@@ -35,4 +36,21 @@ export function feltsToBytes(felts: BN[]): Buffer {
   }
 
   return Buffer.from(data)
+}
+
+export const decodeOffchainConfigFromEventData = (data: string[]): encoding.OffchainConfig => {
+  const oraclesLen = number.toBN(data[3]).toNumber()
+  /** SetConfig event data has the following info:
+    previous_config_block_number=prev_block_num,
+    latest_config_digest=digest,
+    config_count=config_count,
+    oracles_len=oracles_len, // It includes both signer and transmitter addresses
+    oracles=oracles,
+    f=f,
+    onchain_config=onchain_config,
+    offchain_config_version=offchain_config_version,
+    offchain_config_len=offchain_config_len,
+   */
+  const offchainConfigFelts = data.slice(3 + oraclesLen * 2 + 5)
+  return encoding.deserializeConfig(feltsToBytes(offchainConfigFelts.map((f) => number.toBN(f))))
 }
