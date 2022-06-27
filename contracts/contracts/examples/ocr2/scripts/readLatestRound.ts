c6b95e9d1250a0c03a18abe53aad39f9da191a90
qperrot <56728586+qperrot@users.noreply.github.com>
2022-06-27 23:15:27 +0900

    OCR2 examples
    Squashed commit of the following:
    
    commit 200c51f8289830ad3f59bb41922d4e90bbca2195
    Author: qperrot <quentinperrotm@gmail.com>
    Date:   Fri Jun 17 11:49:00 2022 +0200
    
        new starknet version and test
    
    commit b3d9e6502d26e89b8aec39eebf1ce9e588d93d54
    Author: qperrot <quentinperrotm@gmail.com>
    Date:   Thu Jun 16 14:05:20 2022 +0200
    
        add round_id in readContinuously and readLatestRound
    
    commit 14a061daa891af3c156c8ee8b0dfb88e61748938
    Author: qperrot <quentinperrotm@gmail.com>
    Date:   Thu Jun 16 11:27:00 2022 +0200
    
        new consumer contract without store
    
    commit 6f139f6f42da8bbb8eb80d2dc2d513cf56934933
    Merge: 524fd04 5055643
    Author: qperrot <quentinperrotm@gmail.com>
    Date:   Wed Jun 15 18:08:42 2022 +0200
    
        pull and resolve conflicts
    
    commit 524fd041392c119a3ad4651544662afe997f1fff
    Author: qperrot <quentinperrotm@gmail.com>
    Date:   Wed Jun 15 18:02:02 2022 +0200
    
        consumer contract and mock contract with scripte
    
    commit 20669b31f2954b289ae3acd802386a42f7d91076
    Author: qperrot <quentinperrotm@gmail.com>
    Date:   Thu Jun 2 10:55:30 2022 +0200
    
        ocr2 examples contracts
    
    commit 8380c81a17cf9e3a7831798973c2aad5e471047c
    Author: qperrot <quentinperrotm@gmail.com>
    Date:   Wed Jun 1 11:03:33 2022 +0200
    
        test clean
    
    commit 7e9584f18a1f70494b18601ff900002243ab9883
    Author: qperrot <quentinperrotm@gmail.com>
    Date:   Wed Jun 1 11:00:20 2022 +0200
    
        npm starkgate and test
    
    commit 50556430d0e9878bfb52dd9b4c6d61c1cd1e5560
    Author: qperrot <quentinperrotm@gmail.com>
    Date:   Thu Jun 2 10:55:30 2022 +0200
    
        ocr2 examples contracts
    
    commit 5b6a2281ad57c70f9e4b9f3c5072d4c7eca8218e
    Author: qperrot <quentinperrotm@gmail.com>
    Date:   Wed Jun 1 11:03:33 2022 +0200
    
        test clean
    
    commit 1ae0596349f7299cd0e5431a89a39bda72b32988
    Author: qperrot <quentinperrotm@gmail.com>
    Date:   Wed Jun 1 11:00:20 2022 +0200
    
        npm starkgate and test

---
+++ contracts/contracts/examples/ocr2/scripts/readLatestRound.ts
@@ -0,0 +1,35 @@
+import { Account, Contract, defaultProvider, ec } from 'starknet'
+import { CallContractResponse } from 'starknet/types'
+
+import { loadContract } from './index'
+import dotenv from 'dotenv'
+
+const CONSUMER_NAME = 'OCR2_consumer'
+let account: Account
+let consumer: Contract
+
+dotenv.config({ path: __dirname + '/.env' })
+
+async function main() {
+  const keyPair = ec.getKeyPair(process.env.PRIVATE_KEY as string)
+  account = new Account(defaultProvider, process.env.ACCOUNT_ADDRESS as string, keyPair)
+  const OCR2Artifact = loadContract(CONSUMER_NAME)
+  consumer = new Contract(OCR2Artifact.abi, process.env.CONSUMER as string)
+
+  const latestRound = await account.callContract({
+    contractAddress: consumer.address,
+    entrypoint: 'readLatestRound',
+    calldata: [],
+  })
+  printResult(latestRound)
+}
+
+function printResult(latestRound: CallContractResponse) {
+  console.log('\nround_id= ', parseInt(latestRound.result[0], 16))
+  console.log('answer= ', parseInt(latestRound.result[1], 16))
+  console.log('block_num= ', parseInt(latestRound.result[2], 16))
+  console.log('observation_timestamp= ', parseInt(latestRound.result[3], 16))
+  console.log('transmission_timestamp= ', parseInt(latestRound.result[4], 16))
+}
+
+main()
