2b66998cb474ee5b6047b718da2bfcf3aa1e8706
Aaron Lu <50029043+aalu1418@users.noreply.github.com>
2022-07-25 16:42:09 -0400

    port keystore from core + keystore updates (#55)
    * add keys + tests from core
    
    * rename keys + remove redundant funcs
    
    * fix key derivation test case
    
    * update ocr2 sig: pubkey + r + s
    
    * fix rebase damage
    
    * update TODO comment for report hashing
    
    * use transmissionsCache in transmitter - instead of direct client
    
    * finalize medianreport codec + split as separate utility
    
    * fix broken docs link

--- relayer/pkg/chainlink/keys/starkkey_test.go
+++ relayer/pkg/chainlink/keys/starkkey_test.go
@@ -10,12 +10,23 @@ import (
 
 func TestStarkKey_PublicKeyStr(t *testing.T) {
 	// randomly generated private key (for testing only)
-	privKey := "0x04c4bfa272a15b2f00f36a72af9db41701e14d40b14621aa60ce3e34cfd2428b"
-	contractAddr := "0x001b1ae54cd4d718b7ae4b689ffc52d260061c6ba9304bbb229b28b1aeb410da"
+	keys := []struct {
+		name     string
+		priv     string
+		contract string
+	}{
+		{"0", "0x0398eca85a333bc5de78f87d70d26f6e1f2438da6d163424b20f6190d3c38a21", "0x057605d472e1478b66396d8abec8f6c58348d9278d25049d9d73dafab40cde0c"},
+		{"1", "0x18e693006a3dc4db5adf7812c2e4ab8d7729707fcb3c439de0939f39de8d2b", "0x05c96456a9d58aa45e997182050f07a0649638a8f1d955935b42b6898d99e63d"},
+		{"2", "0x0358549759856b585a7b74ce5462e0ec0e56dbcc8fc729255150da2b62b702a6", "0x025d26785bc488193674b4e504f1ea0fc0bc28b0b92b7ce3e4b63ea5514bc3ab"},
+	}
 
-	privBytes, err := hex.DecodeString(privKey[2:])
-	require.NoError(t, err)
+	for _, k := range keys {
+		t.Run(k.name, func(t *testing.T) {
+			b, err := hex.DecodeString(k.priv[2:])
+			require.NoError(t, err)
+			key := StarkRaw(b).Key()
 
-	key := Raw(privBytes).Key()
-	assert.Equal(t, contractAddr, key.PublicKeyStr(), "address calculated from private key does not match expected contract address")
+			assert.Equal(t, k.contract, key.PublicKeyStr(), "address calculated from private key does not match expected contract address")
+		})
+	}
 }
