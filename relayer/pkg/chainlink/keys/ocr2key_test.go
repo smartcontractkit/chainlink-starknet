efb37174cbc5459671a187050707f783521bda77
Aaron Lu <50029043+aalu1418@users.noreply.github.com>
2022-07-26 12:52:35 -0400

    export ocr2 keys (#58)
    * export ocr2 keys
    
    * export Marshal/Unmarshal

--- relayer/pkg/chainlink/keys/ocr2key_test.go
+++ relayer/pkg/chainlink/keys/ocr2key_test.go
@@ -10,9 +10,9 @@ import (
 )
 
 func TestStarkNetKeyring_Sign_Verify(t *testing.T) {
-	kr1, err := newStarkNetKeyring(cryptorand.Reader)
+	kr1, err := NewStarkNetKeyring(cryptorand.Reader)
 	require.NoError(t, err)
-	kr2, err := newStarkNetKeyring(cryptorand.Reader)
+	kr2, err := NewStarkNetKeyring(cryptorand.Reader)
 	require.NoError(t, err)
 	ctx := ocrtypes.ReportContext{}
 	report := ocrtypes.Report{
@@ -49,15 +49,15 @@ func TestStarkNetKeyring_Sign_Verify(t *testing.T) {
 }
 
 func TestStarkNetKeyring_Marshal(t *testing.T) {
-	kr1, err := newStarkNetKeyring(cryptorand.Reader)
+	kr1, err := NewStarkNetKeyring(cryptorand.Reader)
 	require.NoError(t, err)
-	m, err := kr1.marshal()
+	m, err := kr1.Marshal()
 	require.NoError(t, err)
-	kr2 := ocr2Keyring{}
-	err = kr2.unmarshal(m)
+	kr2 := OCR2Keyring{}
+	err = kr2.Unmarshal(m)
 	require.NoError(t, err)
 	assert.True(t, kr1.privateKey.Equal(&kr2.privateKey))
 
 	// Invalid seed size should error
-	require.Error(t, kr2.unmarshal([]byte{0x01}))
+	require.Error(t, kr2.Unmarshal([]byte{0x01}))
 }
