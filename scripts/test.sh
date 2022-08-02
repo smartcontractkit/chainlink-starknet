c2a6f813f1ff9e078bebeb024910e78d3c775993
qperrot <quentinperrotm@gmail.com>
2022-08-02 10:50:09 +0200

    test should pass with new config

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-cd contracts
-yarn install
-# Remove once https://github.com/Shard-Labs/starknet-hardhat-plugin/pull/106 is merged
-npx hardhat starknet-compile
-yarn test
-
-# Validator tests
-cd ..
 sh ./integration-tests/scripts/devnet-hardhat.sh
 
 # Example tests
