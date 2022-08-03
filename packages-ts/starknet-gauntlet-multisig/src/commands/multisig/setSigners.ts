313bee0fb773d49047c587d509a3da26b17638ec
Blaž Hrastnik <blaz@mxxn.io>
2022-08-03 14:00:11 +0900

    multisig: owners was renamed to signers

--- packages-ts/starknet-gauntlet-multisig/src/commands/multisig/setSigners.ts
+++ packages-ts/starknet-gauntlet-multisig/src/commands/multisig/setSigners.ts
@@ -2,44 +2,44 @@ import { BeforeExecute, ExecuteCommandConfig, ExecutionContext, makeExecuteComma
 import { number } from 'starknet'
 import { CATEGORIES } from '../../lib/categories'
 import { contractLoader } from '../../lib/contracts'
-import { validateOwners } from './deploy'
+import { validateSigners } from './deploy'
 
 type UserInput = {
-  owners: string[]
+  signers: string[]
 }
 
-type ContractInput = [owners: string[]]
+type ContractInput = [signers: string[]]
 
 const makeUserInput = async (flags, args): Promise<UserInput> => {
   if (flags.input) return flags.input as UserInput
 
   return {
-    owners: flags.owners,
+    signers: flags.signers,
   }
 }
 
 const makeContractInput = async (input: UserInput): Promise<ContractInput> => {
-  return [input.owners.map((addr) => number.toFelt(number.toBN(addr)))]
+  return [input.signers.map((addr) => number.toFelt(number.toBN(addr)))]
 }
 
 const beforeExecute: BeforeExecute<UserInput, ContractInput> = (context, input, deps) => async () => {
   deps.logger
-    .info(`About to set owners on the Multisig Contract (${context.contractAddress}) with the following details:
-    - New Owners: ${input.user.owners}
+    .info(`About to set signers on the Multisig Contract (${context.contractAddress}) with the following details:
+    - New Signers: ${input.user.signers}
   `)
 }
 
 const commandConfig: ExecuteCommandConfig<UserInput, ContractInput> = {
   ux: {
-    description: 'Set owners of the multisig account',
-    examples: [`${CATEGORIES.MULTISIG}:set_owners --network=<NETWORK> --owners=[OWNERS_LIST]`],
+    description: 'Set signers of the multisig account',
+    examples: [`${CATEGORIES.MULTISIG}:set_signers --network=<NETWORK> --signers=[SIGNERS_LIST]`],
   },
   category: CATEGORIES.MULTISIG,
-  action: 'set_owners',
+  action: 'set_signers',
   contractId: CATEGORIES.MULTISIG,
   makeUserInput,
   makeContractInput,
-  validations: [validateOwners],
+  validations: [validateSigners],
   loadContract: contractLoader,
   hooks: {
     beforeExecute,
