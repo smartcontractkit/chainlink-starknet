9d3acfeea351348eb6971c0f927d4e3724b5b3db
qperrot <quentinperrotm@gmail.com>
2022-08-04 19:15:53 +0200

    new with vendor

--- contracts/contracts/solidity/StarknetValidator.sol
+++ contracts/contracts/solidity/StarknetValidator.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity >0.6.0 <=0.8.0;
 
 import '@chainlink/contracts/src/v0.8/interfaces/AggregatorValidatorInterface.sol';
 import '@chainlink/contracts/src/v0.8/interfaces/TypeAndVersionInterface.sol';
