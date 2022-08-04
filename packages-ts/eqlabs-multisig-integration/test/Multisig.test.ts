b0f31e6bd596adc3e13f6227b594d4feaf823da6
edwin <edwinswatpako@gmail.com>
2022-08-04 17:26:55 +0300

    Update multisig artifacts, tests + removed unnecessary files

--- packages-ts/eqlabs-multisig-integration/test/Multisig.test.ts
+++ packages-ts/eqlabs-multisig-integration/test/Multisig.test.ts
@@ -1,58 +1,65 @@
-import { expect } from "chai";
-import { starknet } from "hardhat";
-import {
-    StarknetContract,
-    StarknetContractFactory,
-    Account,
-} from "hardhat/types/runtime";
-import { getSelectorFromName } from "starknet/dist/utils/hash";
-import { number } from "starknet";
+import { expect } from 'chai'
+import { starknet } from 'hardhat'
+import { StarknetContract, StarknetContractFactory, Account } from 'hardhat/types/runtime'
+import { getSelectorFromName } from 'starknet/dist/utils/hash'
+import { number } from 'starknet'
 
-describe("Multisig integration tests", function () {
-    this.timeout(300_000);
+describe('Multisig integration tests', function () {
+  this.timeout(300_000)
 
-    let multisigFactory: StarknetContractFactory;
-    let multisig: StarknetContract;
-    let account: Account;
+  let account1: Account
+  let account2: Account
+  let account3: Account
 
-    let txIndex = -1; // faster to track this internally than to request from contract
+  let multisig: StarknetContract
 
-    before(async () => {
-        multisigFactory = await starknet.getContractFactory("Multisig");
-        account = await starknet.deployAccount("OpenZeppelin");
+  before(async function () {
+    account1 = await starknet.deployAccount('OpenZeppelin')
+    account2 = await starknet.deployAccount('OpenZeppelin')
+    account3 = await starknet.deployAccount('OpenZeppelin')
+  })
+
+  it('Deploy contract', async () => {
+    let multisigFactory = await starknet.getContractFactory('Multisig')
+    multisig = await multisigFactory.deploy({
+      signers: [
+        number.toBN(account1.starknetContract.address),
+        number.toBN(account2.starknetContract.address),
+        number.toBN(account3.starknetContract.address),
+      ],
+      threshold: 2,
     })
 
-    it('Deploy contract', async () => {
-        multisig = await multisigFactory.deploy({
-            owners: [account.starknetContract.address],
-            confirmations_required: 1
-        });
+    expect(multisig).to.be.ok
+  })
 
-        expect(multisig).to.be.ok;
+  it('should submit & confirm transaction', async () => {
+    const nonce = 0
+    const selector = getSelectorFromName('get_signers')
+    const payload = {
+      to: multisig.address,
+      function_selector: selector,
+      calldata: [],
+      nonce,
+    }
+
+    const res = await account1.invoke(multisig, 'submit_transaction', payload)
+    const txReciept = await starknet.getTransactionReceipt(res)
+
+    expect(txReciept.events.length).to.equal(1)
+    expect(txReciept.events[0].data.length).to.equal(3)
+    expect(txReciept.events[0].data[1]).to.equal(number.toHex(number.toBN(nonce, 'hex')))
+
+    await account1.invoke(multisig, 'confirm_transaction', {
+      nonce,
     })
 
-    it('should submit & confirm transaction', async () => {
-        txIndex++;
-        const selector = getSelectorFromName("get_owners");
-        const payload = {
-            to: multisig.address,
-            function_selector: selector,
-            calldata: [],
-        }
-
-        const res = await account.invoke(multisig, "submit_transaction", payload);
-        const txReciept = await starknet.getTransactionReceipt(res);
-
-        expect(txReciept.events.length).to.equal(1);
-        expect(txReciept.events[0].data.length).to.equal(3);
-        expect(txReciept.events[0].data[1]).to.equal(number.toHex(number.toBN(txIndex, "hex")));
-
-        await account.invoke(multisig, 'confirm_transaction', {
-            tx_index: txIndex
-        });
-
-        await account.invoke(multisig, 'execute_transaction', {
-            tx_index: txIndex
-        });
+    await account2.invoke(multisig, 'confirm_transaction', {
+      nonce,
     })
+
+    await account3.invoke(multisig, 'execute_transaction', {
+      nonce,
+    })
+  })
 })
