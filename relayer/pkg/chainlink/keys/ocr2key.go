7b5129c2ce5145d4230840c541d58208e7cc9ac8
Blaž Hrastnik <blaz@mxxn.io>
2022-08-12 14:06:07 +0900

    Temporarily blank out ExtraHash

--- relayer/pkg/chainlink/keys/ocr2key.go
+++ relayer/pkg/chainlink/keys/ocr2key.go
@@ -40,7 +40,8 @@ func (sk *OCR2Key) reportToSigData(reportCtx ocrtypes.ReportContext, report ocrt
 	rawReportContext := evmutil.RawReportContext(reportCtx)
 	dataArray = append(dataArray, new(big.Int).SetBytes(rawReportContext[0][:]))
 	dataArray = append(dataArray, new(big.Int).SetBytes(rawReportContext[1][:]))
-	dataArray = append(dataArray, new(big.Int).SetBytes(rawReportContext[2][:]))
+	// dataArray = append(dataArray, new(big.Int).SetBytes(rawReportContext[2][:]))
+	dataArray = append(dataArray, new(big.Int)) // TODO: extraBytes can overflow felts
 
 	// split report into seperate felts for hashing
 	splitReport, err := medianreport.SplitReport(report)
