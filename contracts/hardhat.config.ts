adf2c3a6432b2986ae3f05419a17038949d93c15
edwin <edwinswatpako@gmail.com>
2022-08-13 17:13:33 +0300

    Revert hardhat.config.ts

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -37,7 +37,7 @@ const config: HardhatUserConfig = {
     // uses the currently active Python environment (hopefully with available Starknet commands!)
     venv: 'active',
     // network: "alpha",
-    network: 'devnet',
+    network: 'integrated-devnet',
     wallets: {
       OpenZeppelin: {
         accountName: 'OpenZeppelin',
