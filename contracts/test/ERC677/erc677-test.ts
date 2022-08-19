09242e78de3ea42fb964e5494c79633a0764601b
qperrot <quentinperrotm@gmail.com>
2022-08-19 20:07:43 +0200

    format fix with new name

--- contracts/test/ERC677/erc677-test.ts
+++ contracts/test/ERC677/erc677-test.ts
@@ -8,122 +8,122 @@ import { Account, StarknetContract, StarknetContractFactory } from 'hardhat/type
 import { TIMEOUT } from '../constants'
 
 function uint256ToBigInt(uint256: { low: bigint; high: bigint }) {
-    return new BN(((BigInt(uint256.high) << BigInt(128)) + BigInt(uint256.low)).toString())
+  return new BN(((BigInt(uint256.high) << BigInt(128)) + BigInt(uint256.low)).toString())
 }
 
 describe('ERC677', function () {
-    this.timeout(TIMEOUT)
+  this.timeout(TIMEOUT)
 
-    let receiverFactory: StarknetContractFactory
-    let tokenFactory: StarknetContractFactory
+  let receiverFactory: StarknetContractFactory
+  let tokenFactory: StarknetContractFactory
 
-    let receiver: StarknetContract
-    let sender: Account
-    let token: StarknetContract
-    let data: (number | bigint)[]
+  let receiver: StarknetContract
+  let sender: Account
+  let token: StarknetContract
+  let data: (number | bigint)[]
+
+  beforeEach(async () => {
+    sender = await starknet.deployAccount('OpenZeppelin')
+
+    receiverFactory = await starknet.getContractFactory('token677_receiver_mock')
+    tokenFactory = await starknet.getContractFactory('link_token')
+
+    receiver = await receiverFactory.deploy({})
+    token = await tokenFactory.deploy({ minter: sender.starknetContract.address })
+
+    await sender.invoke(token, 'permissionedMint', {
+      recipient: sender.starknetContract.address,
+      amount: { high: 0n, low: 1000n },
+    })
+    await sender.invoke(token, 'transfer', {
+      recipient: sender.starknetContract.address,
+      amount: { high: 0n, low: 100n },
+    })
+    const { value: value } = await receiver.call('get_sent_value')
+    expect(uint256ToBigInt(value)).to.deep.equal(toBN(0))
+  })
+
+  describe('#transferAndCall(address, uint, bytes)', () => {
+    beforeEach(() => {
+      data = [100n, 0n, 12]
+    })
+
+    it('transfers the tokens', async () => {
+      let { balance: balance } = await token.call('balanceOf', {
+        account: receiver.address,
+      })
+
+      expect(uint256ToBigInt(balance)).to.deep.equal(toBN(0))
+
+      await sender.invoke(token, 'transferAndCall', {
+        to: receiver.address,
+        value: { high: 0n, low: 100n },
+        data: data,
+      })
+
+      let { balance: balance1 } = await token.call('balanceOf', {
+        account: receiver.address,
+      })
+      expect(uint256ToBigInt(balance1)).to.deep.equal(toBN(100))
+    })
+
+    it('calls the token fallback function on transfer', async () => {
+      await sender.invoke(token, 'transferAndCall', {
+        to: receiver.address,
+        value: { high: 0n, low: 100n },
+        data: data,
+      })
+
+      const { bool: bool } = await receiver.call('get_called_fallback', {})
+      expect(bool).to.deep.equal(1n)
+
+      const { address: address } = await receiver.call('get_token_sender', {})
+      expect(address).to.equal(BigInt(sender.starknetContract.address))
+
+      const { value: sentValue } = await receiver.call('get_sent_value')
+      expect(uint256ToBigInt(sentValue)).to.deep.equal(toBN(100))
+    })
+
+    it('transfer succeeds with response', async () => {
+      const response = await sender.invoke(token, 'transferAndCall', {
+        to: receiver.address,
+        value: { high: 0n, low: 100n },
+        data: data,
+      })
+      expect(response).to.exist
+    })
+
+    it('throws when the transfer fails', async () => {
+      try {
+        await sender.invoke(token, 'transfer', { recipient: receiver.address, amount: { high: 0n, low: 10000n } })
+        expect.fail()
+      } catch (error: any) {}
+    })
+  })
+
+  describe('when sending to a contract that is not ERC677 compatible', () => {
+    let nonERC677: StarknetContract
 
     beforeEach(async () => {
-        sender = await starknet.deployAccount('OpenZeppelin')
-
-        receiverFactory = await starknet.getContractFactory('token677_receiver_mock')
-        tokenFactory = await starknet.getContractFactory('link_token')
-
-        receiver = await receiverFactory.deploy({})
-        token = await tokenFactory.deploy({ minter: sender.starknetContract.address })
-
-        await sender.invoke(token, 'permissionedMint', {
-            recipient: sender.starknetContract.address,
-            amount: { high: 0n, low: 1000n },
-        })
-        await sender.invoke(token, 'transfer', {
-            recipient: sender.starknetContract.address,
-            amount: { high: 0n, low: 100n },
-        })
-        const { value: value } = await receiver.call('get_sent_value')
-        expect(uint256ToBigInt(value)).to.deep.equal(toBN(0))
+      const nonERC677Factory = await starknet.getContractFactory('not_erc677_compatible')
+      nonERC677 = await nonERC677Factory.deploy({})
+      data = [1000n, 0n, 12n]
     })
 
-    describe('#transferAndCall(address, uint, bytes)', () => {
-        beforeEach(() => {
-            data = [100n, 0n, 12]
+    it('throws an error', async () => {
+      try {
+        await sender.invoke(token, 'transferAndCall', {
+          to: nonERC677.address,
+          value: { high: 0n, low: 1000n },
+          data: data,
         })
-
-        it('transfers the tokens', async () => {
-            let { balance: balance } = await token.call('balanceOf', {
-                account: receiver.address,
-            })
-
-            expect(uint256ToBigInt(balance)).to.deep.equal(toBN(0))
-
-            await sender.invoke(token, 'transferAndCall', {
-                to: receiver.address,
-                value: { high: 0n, low: 100n },
-                data: data,
-            })
-
-            let { balance: balance1 } = await token.call('balanceOf', {
-                account: receiver.address,
-            })
-            expect(uint256ToBigInt(balance1)).to.deep.equal(toBN(100))
+        expect.fail()
+      } catch (error: any) {
+        let { balance: balance1 } = await token.call('balanceOf', {
+          account: nonERC677.address,
         })
-
-        it('calls the token fallback function on transfer', async () => {
-            await sender.invoke(token, 'transferAndCall', {
-                to: receiver.address,
-                value: { high: 0n, low: 100n },
-                data: data,
-            })
-
-            const { bool: bool } = await receiver.call('get_called_fallback', {})
-            expect(bool).to.deep.equal(1n)
-
-            const { address: address } = await receiver.call('get_token_sender', {})
-            expect(address).to.equal(BigInt(sender.starknetContract.address))
-
-            const { value: sentValue } = await receiver.call('get_sent_value')
-            expect(uint256ToBigInt(sentValue)).to.deep.equal(toBN(100))
-        })
-
-        it('transfer succeeds with response', async () => {
-            const response = await sender.invoke(token, 'transferAndCall', {
-                to: receiver.address,
-                value: { high: 0n, low: 100n },
-                data: data,
-            })
-            expect(response).to.exist
-        })
-
-        it('throws when the transfer fails', async () => {
-            try {
-                await sender.invoke(token, 'transfer', { recipient: receiver.address, amount: { high: 0n, low: 10000n } })
-                expect.fail()
-            } catch (error: any) { }
-        })
+        expect(uint256ToBigInt(balance1)).to.deep.equal(toBN(0))
+      }
     })
-
-    describe('when sending to a contract that is not ERC677 compatible', () => {
-        let nonERC677: StarknetContract
-
-        beforeEach(async () => {
-            const nonERC677Factory = await starknet.getContractFactory('not_erc677_compatible')
-            nonERC677 = await nonERC677Factory.deploy({})
-            data = [1000n, 0n, 12n]
-        })
-
-        it('throws an error', async () => {
-            try {
-                await sender.invoke(token, 'transferAndCall', {
-                    to: nonERC677.address,
-                    value: { high: 0n, low: 1000n },
-                    data: data,
-                })
-                expect.fail()
-            } catch (error: any) {
-                let { balance: balance1 } = await token.call('balanceOf', {
-                    account: nonERC677.address,
-                })
-                expect(uint256ToBigInt(balance1)).to.deep.equal(toBN(0))
-            }
-        })
-    })
+  })
 })
