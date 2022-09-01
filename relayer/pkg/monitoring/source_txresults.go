a2a0078fa93fdbcb761052575dc3bb8b16972803
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2022-09-01 18:50:54 +0100

    refactor the txresults source

--- relayer/pkg/monitoring/source_txresults.go
+++ relayer/pkg/monitoring/source_txresults.go
@@ -3,38 +3,44 @@ package monitoring
 import (
 	"context"
 	"fmt"
+	"sync"
 
 	relayMonitoring "github.com/smartcontractkit/chainlink-relay/pkg/monitoring"
+	"github.com/smartcontractkit/chainlink-starknet/relayer/pkg/monitoring/encoding"
 	"github.com/smartcontractkit/chainlink-starknet/relayer/pkg/starknet"
 )
 
 func NewTxResultsSourceFactory(
-	client starknet.Reader,
+	starknetClient *starknet.Client,
 	log relayMonitoring.Logger,
 ) relayMonitoring.SourceFactory {
-	return &txResultsSourceFactory{
-		client,
+	return &proxySourceFactory{
+		starknetClient,
 		log,
 	}
 }
 
 type txResultsSourceFactory struct {
-	client starknet.Reader
-	log    relayMonitoring.Logger
+	starknetClient *starknet.Client
+	log            relayMonitoring.Logger
 }
 
 func (s *txResultsSourceFactory) NewSource(
-	_ relayMonitoring.ChainConfig,
+	chainConfig relayMonitoring.ChainConfig,
 	feedConfig relayMonitoring.FeedConfig,
 ) (relayMonitoring.Source, error) {
+	starknetChainConfig, ok := chainConfig.(StarknetConfig)
+	if !ok {
+		return nil, fmt.Errorf("expected feedConfig to be of type StarknetFeedConfig not %T", feedConfig)
+	}
 	starknetFeedConfig, ok := feedConfig.(StarknetFeedConfig)
 	if !ok {
-		return nil, fmt.Errorf("expected feedConfig to be of type StarkNetFeedConfig not %T", feedConfig)
+		return nil, fmt.Errorf("expected feedConfig to be of type StarknetFeedConfig not %T", feedConfig)
 	}
-	return &txResultsSource{
-		s.client,
-		s.log,
+	return &proxySource{
+		starknetChainConfig,
 		starknetFeedConfig,
+		s.starknetClient,
 	}, nil
 }
 
@@ -43,11 +49,32 @@ func (s *txResultsSourceFactory) GetType() string {
 }
 
 type txResultsSource struct {
-	client     starknet.Reader
-	log        relayMonitoring.Logger
-	feedConfig StarknetFeedConfig
+	chainConfig    StarknetConfig
+	feedConfig     StarknetFeedConfig
+	starknetClient *starknet.Client
+
+	prevRoundID   uint32
+	prevRoundIDMu sync.Mutex
 }
 
-func (t *txResultsSource) Fetch(ctx context.Context) (interface{}, error) {
-	return relayMonitoring.TxResults{}, nil
+func (s *txResultsSource) Fetch(ctx context.Context) (interface{}, error) {
+	results, err := s.starknetClient.CallContract(ctx, starknet.CallOps{
+		ContractAddress: s.feedConfig.ContractAddress,
+		Selector:        encoding.LatestRoundDataViewName,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("couldn't call the contract selector %s on contract %s: %w", encoding.LatestRoundDataViewName, s.feedConfig.ContractAddress, err)
+	}
+	var latestRoundData encoding.RoundData
+	if err := latestRoundData.Unmarshal(results); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal LatestTransmissionDetails from %v: %w", results, err)
+	}
+	s.prevRoundIDMu.Lock()
+	defer s.prevRoundIDMu.Unlock()
+	var numSucceeded uint32
+	if s.prevRoundID != 0 {
+		numSucceeded = latestRoundData.RoundID - s.prevRoundID
+		s.prevRoundID = latestRoundData.RoundID
+	}
+	return relayMonitoring.TxResults{NumSucceeded: uint64(numSucceeded), NumFailed: 0}, nil
 }
