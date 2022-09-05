0709c88f9e28a3e06bd80d29f656474ee9114782
Blaž Hrastnik <blaz@mxxn.io>
2022-09-05 09:43:57 +0900

    gauntlet: fix package so it includes correct contracts

--- packages-ts/starknet-gauntlet-ocr2/src/lib/contracts.ts
+++ packages-ts/starknet-gauntlet-ocr2/src/lib/contracts.ts
@@ -3,7 +3,7 @@ import { CompiledContract, json } from 'starknet'
 
 export enum CONTRACT_LIST {
   OCR2 = 'aggregator',
-  ACCESS_CONTROLLER = 'access_controller',
+  ACCESS_CONTROLLER = 'simple_write_access_controller',
 }
 
 export const loadContractOcr2 = (name: CONTRACT_LIST): CompiledContract => {
@@ -20,7 +20,7 @@ export const loadContractAccessController = (name: CONTRACT_LIST): CompiledContr
   return json.parse(
     fs
       .readFileSync(
-        `${__dirname}/../../../../node_modules/@chainlink-dev/starknet-contracts-ocr2/artifacts/${name}.cairo/${name}.json`,
+        `${__dirname}/../../../../node_modules/@chainlink-dev/starknet-contracts-ocr2/artifacts/access/SimpleWriteAccessController/${name}.cairo/${name}.json`,
       )
       .toString('ascii'),
   )
