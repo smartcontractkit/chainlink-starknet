468eb3d48bb43f43c5ccf3edffcfe9756a5cb6a2
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2022-09-05 15:12:18 +0100

    add boilerplate to run and build the monitor

---
+++ relayer/cmd/monitoring/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/smartcontractkit/chainlink-relay/pkg/logger"
+	relayMonitoring "github.com/smartcontractkit/chainlink-relay/pkg/monitoring"
+	"github.com/smartcontractkit/chainlink-starknet/relayer/pkg/chainlink/ocr2"
+	"github.com/smartcontractkit/chainlink-starknet/relayer/pkg/starknet"
+
+	"github.com/smartcontractkit/chainlink-starknet/relayer/pkg/monitoring"
+)
+
+func main() {
+	ctx := context.Background()
+
+	log, err := logger.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if serr := log.Sync(); serr != nil {
+			fmt.Printf("Error while closing Logger: %v\n", serr)
+		}
+	}()
+
+	starknetConfig, err := monitoring.ParseStarknetConfig()
+	if err != nil {
+		log.Fatalw("failed to parse starknet specific configuration", "error", err)
+		return
+	}
+
+	starknetClient, err := starknet.NewClient(
+		starknetConfig.ChainID,
+		starknetConfig.RPCEndpoint,
+		logger.With(log, "component", "starknet-client"),
+		&starknetConfig.ReadTimeout,
+	)
+	if err != nil {
+		log.Fatalw("failed to build a starknet.Client", "error", err)
+	}
+	ocr2Client, err := ocr2.NewClient(
+		starknetClient,
+		logger.With(log, "component", "ocr2-client"),
+	)
+	if err != nil {
+		log.Fatalw("failed to build a ocr2.Client", "error", err)
+	}
+
+	envelopeSourceFactory := monitoring.NewEnvelopeSourceFactory(ocr2Client)
+	txResultsFactory := monitoring.NewTxResultsSourceFactory(ocr2Client)
+
+	monitor, err := relayMonitoring.NewMonitor(
+		ctx,
+		logger.With(log, "component", "monitor"),
+		starknetConfig,
+		envelopeSourceFactory,
+		txResultsFactory,
+		monitoring.StarknetFeedsParser,
+		monitoring.StarknetNodesParser,
+	)
+	if err != nil {
+		log.Fatalw("failed to build monitor", "error", err)
+		return
+	}
+
+	proxySourceFactory := monitoring.NewProxySourceFactory(ocr2Client)
+	monitor.SourceFactories = append(monitor.SourceFactories, proxySourceFactory)
+
+	prometheusExporterFactory := monitoring.NewPrometheusExporterFactory(
+		monitoring.NewMetrics(logger.With(log, "component", "starknet-metrics")),
+	)
+	monitor.ExporterFactories = append(monitor.ExporterFactories, prometheusExporterFactory)
+
+	monitor.Run()
+	log.Info("monitor stopped")
+}
