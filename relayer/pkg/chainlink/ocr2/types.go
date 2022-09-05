32289081baf8c558b862182a7f6a142106b69c8f
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2022-09-05 13:27:49 +0100

    add method to fetch latest RoundData in the ocr2#Client

--- relayer/pkg/chainlink/ocr2/types.go
+++ relayer/pkg/chainlink/ocr2/types.go
@@ -1,6 +1,8 @@
 package ocr2
 
 import (
+	"encoding/binary"
+	"fmt"
 	"math/big"
 	"time"
 
@@ -56,4 +58,36 @@ func NewBillingDetails(observationPaymentFelt junotypes.Felt, transmissionPaymen
 		ObservationPaymentGJuels:  observationPaymentGJuels.Uint64(),
 		TransmissionPaymentGJuels: transmissionPaymentGJuels.Uint64(),
 	}, nil
+}
+
+type RoundData struct {
+	RoundID     uint32
+	Answer      *big.Int
+	BlockNumber uint64
+	StartedAt   time.Time
+	UpdatedAt   time.Time
+}
+
+func NewRoundData(felts []junotypes.Felt) (data RoundData, err error) {
+	if len(felts) < 5 {
+		return data, fmt.Errorf("expected number of felts to be 5 but got %d", len(felts))
+	}
+	data.RoundID = binary.BigEndian.Uint32(felts[0][:])
+	data.Answer = felts[1].Big()
+	blockNumber := felts[2].Big()
+	if !blockNumber.IsUint64() {
+		return data, fmt.Errorf("block number '%s' does not fit into uint64", blockNumber.String())
+	}
+	data.BlockNumber = blockNumber.Uint64()
+	startedAt := felts[3].Big()
+	if !startedAt.IsInt64() {
+		return data, fmt.Errorf("startedAt '%s' does not fit into int64", startedAt.String())
+	}
+	data.StartedAt = time.Unix(startedAt.Int64(), 0)
+	updatedAt := felts[3].Big()
+	if !updatedAt.IsInt64() {
+		return data, fmt.Errorf("updatedAt '%s' does not fit into int64", startedAt.String())
+	}
+	data.StartedAt = time.Unix(startedAt.Int64(), 0)
+	return data, nil
 }
