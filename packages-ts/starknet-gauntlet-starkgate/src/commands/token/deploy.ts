cf0582db792548bf0571e95973beb05f94b8b804
Sergei Drugalev <sergei.drugalev@smartcontract.com>
2022-09-06 15:22:06 +0200

    contractLoader -> tokenContractLoader

--- packages-ts/starknet-gauntlet-starkgate/src/commands/token/deploy.ts
+++ packages-ts/starknet-gauntlet-starkgate/src/commands/token/deploy.ts
@@ -9,7 +9,7 @@ import {
 } from '@chainlink/starknet-gauntlet'
 import { shortString } from 'starknet'
 import { CATEGORIES } from '../../lib/categories'
-import { contractLoader, CONTRACT_LIST } from '../../lib/contracts'
+import { tokenContractLoader, CONTRACT_LIST } from '../../lib/contracts'
 
 type UserInput = {
   name: string
@@ -69,7 +69,7 @@ const commandConfig: ExecuteCommandConfig<UserInput, ContractInput> = {
   makeUserInput,
   makeContractInput,
   validations: [],
-  loadContract: contractLoader,
+  loadContract: tokenContractLoader,
   hooks: {
     beforeExecute,
   },
