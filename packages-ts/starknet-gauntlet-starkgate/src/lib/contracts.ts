047cf052ab09898d7fe43efd30ebdc05940a07f6
Sergei Drugalev <sergei.drugalev@smartcontract.com>
2022-09-06 15:22:56 +0200

    Loading L1 contract artifacts

--- packages-ts/starknet-gauntlet-starkgate/src/lib/contracts.ts
+++ packages-ts/starknet-gauntlet-starkgate/src/lib/contracts.ts
@@ -1,13 +1,17 @@
 import fs from 'fs'
 import { CompiledContract, json } from 'starknet'
+import { ContractFactory } from 'ethers'
 
+// todo: fix name mapping
+// l1_token_bridge = token_bridge
+// l2_token_bridge = StarknetERC20Bridge
 export enum CONTRACT_LIST {
   TOKEN = 'ERC20',
   L1_BRIDGE = 'l1_token_bridge',
   L2_BRIDGE = 'l2_token_bridge',
 }
 
-export const loadContract = (name: CONTRACT_LIST): CompiledContract => {
+export const loadL2Contract = (name: CONTRACT_LIST): CompiledContract => {
   return json.parse(
     fs
       .readFileSync(
@@ -17,6 +21,17 @@ export const loadContract = (name: CONTRACT_LIST): CompiledContract => {
   )
 }
 
-export const contractLoader = () => loadContract(CONTRACT_LIST.TOKEN)
-export const l1BridgeContractLoader = () => loadContract(CONTRACT_LIST.L1_BRIDGE)
-export const l2BridgeContractLoader = () => loadContract(CONTRACT_LIST.L2_BRIDGE)
+export const loadL1BridgeContract = (name: CONTRACT_LIST): ContractFactory => {
+  const abi = JSON.parse(
+    fs
+      .readFileSync(
+        `${__dirname}/../../../../node_modules/internals-starkgate-contracts/artifacts/0.0.3/eth/StarknetERC20Bridge.json`,
+      )
+      .toString('ascii'),
+  )
+  return new ContractFactory(abi?.abi, abi?.bytecode)
+}
+
+export const tokenContractLoader = () => loadL2Contract(CONTRACT_LIST.TOKEN)
+export const l1BridgeContractLoader = () => loadL1BridgeContract(CONTRACT_LIST.L1_BRIDGE)
+export const l2BridgeContractLoader = () => loadL2Contract(CONTRACT_LIST.L2_BRIDGE)
