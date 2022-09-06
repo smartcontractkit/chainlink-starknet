9620ecb6308435c88e0711b7667a5a945099ee14
Kristijan Rebernisak <kristijan.rebernisak@gmail.com>
2022-09-06 14:07:13 +0200

    Add funds withdrawal functionality to L2EP's StarkNetValidator

--- contracts/src/chainlink/solidity/emergency/StarkNetValidator.sol
+++ contracts/src/chainlink/solidity/emergency/StarkNetValidator.sol
@@ -6,17 +6,15 @@ import "@chainlink/contracts/src/v0.8/interfaces/TypeAndVersionInterface.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AccessControllerInterface.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "@chainlink/contracts/src/v0.8/SimpleWriteAccessController.sol";
-
-import "@chainlink/contracts/src/v0.8/dev/vendor/openzeppelin-solidity/v4.3.1/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
 
 import "../../../../vendor/starkware-libs/starkgate-contracts-solidity-v0.8/src/starkware/starknet/solidity/IStarknetMessaging.sol";
 
-/**
- * @title StarkNetValidator - makes cross chain call to update the Sequencer Uptime Feed on L2
- */
+/// @title StarkNetValidator - makes cross chain calls to update the Sequencer Uptime Feed on L2
 contract StarkNetValidator is TypeAndVersionInterface, AggregatorValidatorInterface, SimpleWriteAccessController {
   int256 private constant ANSWER_SEQ_OFFLINE = 1;
-  // Selector hardcoded because StarkNet hash function is not available in this environment
+  // The selector is the starknet_keccak hash of the function name 'update_status'.
+  // Notice: hardcoded b/c starknet_keccak is not available in this environment.
   uint256 constant STARK_SELECTOR_UPDATE_STATUS =
     1585322027166395525705364165097050997465692350398750944680096081848180365267;
 
@@ -27,7 +25,6 @@ contract StarkNetValidator is TypeAndVersionInterface, AggregatorValidatorInterf
 
   /// @notice StarkNet messaging contract address - the address is 0.
   error InvalidStarkNetMessaging();
-
   /// @notice StarkNet uptime feed address - the address is 0.
   error InvalidUptimeFeedAddress();
 
@@ -99,7 +96,8 @@ contract StarkNetValidator is TypeAndVersionInterface, AggregatorValidatorInterf
     payload[0] = toUInt256(status);
     payload[1] = block.timestamp;
     // Make the StarkNet x-domain call.
-    // NOTICE: we ignore the output of this call (msgHash, nonce), and we don't raise any events as the event LogMessageToL2 will be emitted from the messaging contract
+    // NOTICE: we ignore the output of this call (msgHash, nonce).
+    // We also don't raise any events as the 'LogMessageToL2' event will be emitted from the messaging contract.
     STARKNET_CROSS_DOMAIN_MESSENGER.sendMessageToL2{value: s_fee}(
       L2_UPTIME_FEED_ADDR,
       STARK_SELECTOR_UPDATE_STATUS,
@@ -110,8 +108,27 @@ contract StarkNetValidator is TypeAndVersionInterface, AggregatorValidatorInterf
 
   /**
    * @notice makes this contract payable
-   * @dev receives funds:
-   *  - funds are used to send cross chain messages to L2
+   * @dev funds are used to pay the bridge for x-domain messages to L2
    */
   receive() external payable {}
+
+  /**
+   * @notice withdraws all funds available in this contract to the msg.sender
+   * @dev only owner can call this
+   */
+  function withdrawFunds() external onlyOwner {
+    address payable recipient = payable(msg.sender);
+    uint256 amount = address(this).balance;
+    Address.sendValue(recipient, amount);
+  }
+
+  /**
+   * @notice withdraws all funds available in this contract to the address specified
+   * @dev only owner can call this
+   * @param recipient address where to send the funds
+   */
+  function withdrawFundsTo(address payable recipient) external onlyOwner {
+    uint256 amount = address(this).balance;
+    Address.sendValue(recipient, amount);
+  }
 }
