e05cba086725df381b79d52311160443e454d810
Christopher Dimitri Sastropranoto <christopher.sastropranoto@smartcontract.com>
2022-09-06 16:19:45 -0700

    add L1 StarknetValidator commands

--- packages-ts/starknet-gauntlet-emergency-protocol/src/commands/starknetValidator/acceptOwnership.ts
+++ packages-ts/starknet-gauntlet-emergency-protocol/src/commands/starknetValidator/acceptOwnership.ts
@@ -2,32 +2,23 @@ import { EVMExecuteCommandConfig, makeEVMExecuteCommand } from '@chainlink/evm-g
 import { CONTRACT_LIST, starknetValidatorContractLoader } from '../../lib/contracts'
 import { CATEGORIES } from '../../lib/categories'
 
-export interface ContractInput {
-  starkNetMessaging: number
-  l2UptimeFeedAddr: string
-}
+export interface ContractInput {}
 
 const makeContractInput = async (input: ContractInput): Promise<ContractInput> => {
   return input
 }
 
 const makeUserInput = async (flags): Promise<ContractInput> => {
-  if (flags.input) return flags.input as ContractInput
-  return {
-    l2UptimeFeedAddr: flags.l2UptimeFeedAddr,
-    starkNetMessaging: flags.starkNetMessaging,
-  }
+  return {}
 }
 
 const commandConfig: EVMExecuteCommandConfig<ContractInput, ContractInput> = {
   contractId: CONTRACT_LIST.STARKNET_VALIDATOR,
   category: CATEGORIES.STARKNET_VALIDATOR,
-  action: 'deploy',
+  action: 'acceptOwnership',
   ux: {
-    description: 'Deploys a StarknetValidator contract',
-    examples: [
-      `${CATEGORIES.STARKNET_VALIDATOR}:deploy --starkNetMessaging=0xc662c410C0ECf747543f5bA90660f6ABeBD9C8c4 --l2UptimeFeedAddr=0x0646bbfcaab5ead1f025af1e339cb0f2d63b070b1264675da9a70a9a5efd054f --network=<NETWORK>`,
-    ],
+    description: 'Accepts ownership of the StarknetValidator contract',
+    examples: [`${CATEGORIES.STARKNET_VALIDATOR}:acceptOwnership --network=<NETWORK>`],
   },
   makeUserInput,
   makeContractInput,
--- packages-ts/starknet-gauntlet-emergency-protocol/src/commands/starknetValidator/transferOwnership.ts
+++ packages-ts/starknet-gauntlet-emergency-protocol/src/commands/starknetValidator/transferOwnership.ts
@@ -3,8 +3,7 @@ import { CONTRACT_LIST, starknetValidatorContractLoader } from '../../lib/contra
 import { CATEGORIES } from '../../lib/categories'
 
 export interface ContractInput {
-  starkNetMessaging: number
-  l2UptimeFeedAddr: string
+  to: string
 }
 
 const makeContractInput = async (input: ContractInput): Promise<ContractInput> => {
@@ -14,19 +13,18 @@ const makeContractInput = async (input: ContractInput): Promise<ContractInput> =
 const makeUserInput = async (flags): Promise<ContractInput> => {
   if (flags.input) return flags.input as ContractInput
   return {
-    l2UptimeFeedAddr: flags.l2UptimeFeedAddr,
-    starkNetMessaging: flags.starkNetMessaging,
+    to: flags.to,
   }
 }
 
 const commandConfig: EVMExecuteCommandConfig<ContractInput, ContractInput> = {
   contractId: CONTRACT_LIST.STARKNET_VALIDATOR,
   category: CATEGORIES.STARKNET_VALIDATOR,
-  action: 'deploy',
+  action: 'transferOwnership',
   ux: {
-    description: 'Deploys a StarknetValidator contract',
+    description: 'Transfers ownership of the StarknetValidator contract',
     examples: [
-      `${CATEGORIES.STARKNET_VALIDATOR}:deploy --starkNetMessaging=0xc662c410C0ECf747543f5bA90660f6ABeBD9C8c4 --l2UptimeFeedAddr=0x0646bbfcaab5ead1f025af1e339cb0f2d63b070b1264675da9a70a9a5efd054f --network=<NETWORK>`,
+      `${CATEGORIES.STARKNET_VALIDATOR}:transferOwnership --to=0xc662c410C0ECf747543f5bA90660f6ABeBD9C8c4 --network=<NETWORK>`,
     ],
   },
   makeUserInput,
