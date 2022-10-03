b96550bb319c41a0dae3e48b766fa6a0b3938009
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2022-10-03 17:14:35 +0100

    move utility to relayer/pkg/starknet and rename to StringsToJunoFelts

--- relayer/pkg/starknet/utils.go
+++ relayer/pkg/starknet/utils.go
@@ -128,6 +128,14 @@ func StringsToFelt(in []string) (out []*caigotypes.Felt) {
 	return out
 }
 
+func StringsToJunoFelts(in []string) []junotypes.Felt {
+	out := make([]junotypes.Felt, len(in))
+	for i := 0; i < len(in); i++ {
+		out[i] = junotypes.HexToFelt(in[i])
+	}
+	return out
+}
+
 // CompareAddress compares different hex starknet addresses with potentially different 0 padding
 func CompareAddress(a, b string) bool {
 	aBytes, err := caigo.HexToBytes(a)
