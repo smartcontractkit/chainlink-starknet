6d4eb7ec901f5dbdb21b436d1d01e3576fcd006c
Augustus Chang <augustus.chang@smartcontract.com>
2023-01-20 13:05:05 -0500

    add gauntlet tests

--- packages-ts/starknet-gauntlet-emergency-protocol/test/commands/sequencerUptimeFeed.test.ts
+++ packages-ts/starknet-gauntlet-emergency-protocol/test/commands/sequencerUptimeFeed.test.ts
@@ -1,6 +1,9 @@
 import deployCommand from '../../src/commands/sequencerUptimeFeed/deploy'
+import setL1Sender from '../../src/commands/sequencerUptimeFeed/setL1Sender'
+import getLatestRoundData from '../../src/commands/sequencerUptimeFeed/inspection/inspection'
 import {
   registerExecuteCommand,
+  registerInspectCommand,
   TIMEOUT,
   startNetwork,
   IntegratedDevnet,
@@ -8,31 +11,56 @@ import {
 
 describe('Sequencer Uptime Feed Contract', () => {
   let network: IntegratedDevnet
-  let contractAddress: string
+  let uptimeFeedAddress: string
 
   beforeAll(async () => {
     network = await startNetwork()
   }, 15000)
 
-  describe('deploy', () => {
-    it(
-      'Deployment with default input',
-      async () => {
-        const command = await registerExecuteCommand(deployCommand).create(
-          {
-            initialStatus: 0,
-          },
-          [],
-        )
-
-        const report = await command.execute()
-        expect(report.responses[0].tx.status).toEqual('ACCEPTED')
-
-        contractAddress = report.responses[0].contract
+  // deploy uptime feed contract
+  beforeEach(async () => {
+    const command = await registerExecuteCommand(deployCommand).create(
+      {
+        initialStatus: 0,
       },
-      TIMEOUT,
+      [],
     )
+
+    const report = await command.execute()
+    expect(report.responses[0].tx.status).toEqual('ACCEPTED')
+
+    uptimeFeedAddress = report.responses[0].contract
+
+  }, TIMEOUT)
+
+  describe('setL1Sender', () => {
+    it('Sets with a non-Zero valid eth address', async () => {
+      const command = await registerExecuteCommand(setL1Sender).create(
+        {
+          address: '0x0000000000000000000000000000000000000007'
+        },
+        [uptimeFeedAddress],
+      )
+
+      const report = await command.execute()
+      expect(report.responses[0].tx.status).toEqual('ACCEPTED')
+    }, TIMEOUT)
   })
+
+  describe('inspect uptime feed status', () => {
+    it('Retrieves 0', async () => {
+      const inspectCommand = await registerInspectCommand(getLatestRoundData).create(
+        {
+
+        },
+        [uptimeFeedAddress],
+      )
+
+      const report = await inspectCommand.execute()
+      expect(report.data.data.latest_round_data.answer).toEqual('0')
+    })
+  })
+
 
   afterAll(() => {
     network.stop()
