07ad2a13a98362add74feaeeead2512047fc7eea
Blaž Hrastnik <blaz@mxxn.io>
2023-03-30 16:44:49 +0900

    Initial boilerplate for Cairo 1.0
    Based on https://github.com/auditless/cairo-template

--- flake.nix
+++ flake.nix
@@ -4,12 +4,13 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
+  outputs = inputs@{ self, nixpkgs, flake-utils, rust-overlay, ... }:
     flake-utils.lib.eachDefaultSystem (system: 
       let
-        pkgs = import nixpkgs { inherit system; overlays = [ ]; };
+        pkgs = import nixpkgs { inherit system; overlays = [ rust-overlay.overlays.default ]; };        
       in rec {
         devShell = pkgs.callPackage ./shell.nix {};
       });
