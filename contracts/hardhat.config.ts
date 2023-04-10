68a18aa1c4a55adf9087fd96a8a3d4404cad9052
cfal <cfal@users.noreply.github.com>
2023-04-10 17:37:50 +0900

    fix hardhat compilation

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -2,10 +2,6 @@ import { HardhatUserConfig } from 'hardhat/types'
 import '@shardlabs/starknet-hardhat-plugin'
 import '@nomiclabs/hardhat-ethers'
 import '@nomicfoundation/hardhat-chai-matchers'
-import 'solidity-coverage'
-import '@typechain/hardhat'
-import '@nomiclabs/hardhat-ethers'
-import '@nomiclabs/hardhat-waffle'
 
 const COMPILER_SETTINGS = {
   optimizer: {
