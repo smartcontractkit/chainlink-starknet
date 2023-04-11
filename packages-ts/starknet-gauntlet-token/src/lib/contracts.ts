839362624e19ebd02925e2f73097044e58129040
cfal <cfal@users.noreply.github.com>
2023-04-11 14:22:25 +0900

    starknet-gauntlet-token: remove bridge contract references and tests

--- packages-ts/starknet-gauntlet-token/src/lib/contracts.ts
+++ packages-ts/starknet-gauntlet-token/src/lib/contracts.ts
@@ -4,28 +4,11 @@ import { ContractFactory } from 'ethers'
 
 export enum CONTRACT_LIST {
   TOKEN = 'token',
-  L1_BRIDGE = 'l1_bridge',
-  L2_BRIDGE = 'l2_bridge',
-  L1_BRIDGE_PROXY = 'l1_bridge_proxy',
 }
 
 // todo: remove when stable contract artifacts release available
 const CONTRACT_NAME_TO_ARTIFACT_NAME = {
-  [CONTRACT_LIST.L1_BRIDGE]: 'StarknetERC20Bridge',
-  [CONTRACT_LIST.L2_BRIDGE]: 'token_bridge',
   [CONTRACT_LIST.TOKEN]: 'link_token',
-  [CONTRACT_LIST.L1_BRIDGE_PROXY]: 'ERC1967Proxy',
-}
-
-export const loadL2Contract = (name: CONTRACT_LIST): CompiledContract => {
-  const artifactName = CONTRACT_NAME_TO_ARTIFACT_NAME[name]
-  return json.parse(
-    fs
-      .readFileSync(
-        `${__dirname}/../../../../node_modules/@chainlink-dev/starkgate-contracts/artifacts/${artifactName}.cairo/${artifactName}.json`,
-      )
-      .toString('ascii'),
-  )
 }
 
 export const loadTokenContract = (name: CONTRACT_LIST): CompiledContract => {
@@ -37,18 +20,6 @@ export const loadTokenContract = (name: CONTRACT_LIST): CompiledContract => {
       )
       .toString('ascii'),
   )
-}
-
-export const loadL1BridgeContract = (name: CONTRACT_LIST): ContractFactory => {
-  const artifactName = CONTRACT_NAME_TO_ARTIFACT_NAME[name]
-  const abi = JSON.parse(
-    fs
-      .readFileSync(
-        `${__dirname}/../../../../node_modules/internals-starkgate-contracts/artifacts/0.0.3/eth/${artifactName}.json`,
-      )
-      .toString('ascii'),
-  )
-  return new ContractFactory(abi?.abi, abi?.bytecode)
 }
 
 export const loadOZContract = (name: CONTRACT_LIST): any => {
@@ -64,6 +35,3 @@ export const loadOZContract = (name: CONTRACT_LIST): any => {
 }
 
 export const tokenContractLoader = () => loadTokenContract(CONTRACT_LIST.TOKEN)
-export const l1BridgeContractLoader = () => loadL1BridgeContract(CONTRACT_LIST.L1_BRIDGE)
-export const l2BridgeContractLoader = () => loadL2Contract(CONTRACT_LIST.L2_BRIDGE)
-export const l1BridgeProxyContractLoader = () => loadOZContract(CONTRACT_LIST.L1_BRIDGE_PROXY)
