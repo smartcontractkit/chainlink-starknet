8580a2204f65cf6f30afaf667248f9c091d9f68f
cfal <cfal@users.noreply.github.com>
2023-04-24 18:22:00 +0900

    packages-ts/starknet-gauntlet-oz/lib/contracts.ts: remove calculateAddress

--- packages-ts/starknet-gauntlet-oz/src/lib/contracts.ts
+++ packages-ts/starknet-gauntlet-oz/src/lib/contracts.ts
@@ -2,14 +2,6 @@ import fs from 'fs'
 import { CompiledContract, json, hash } from 'starknet'
 import BN from 'bn.js'
 
-// contract hash
-// required in calculation of deployment address: https://docs.starknet.io/docs/Contracts/contract-address/
-// can be calculated using this formula: https://docs.starknet.io/docs/Contracts/contract-hash/
-// easier to deploy an instance then get the hash from: https://alpha4.starknet.io/feeder_gateway/get_class_hash_at?contractAddress=<contract-address>
-// yarn gauntlet account:deploy --network=testnet
-// note: if this value is changed, it must also be changed in the relayer keystore code
-export const CONTRACT_HASH = '0x0750cd490a7cd1572411169eaa8be292325990d33c5d4733655fe6b926985062'
-
 export enum CONTRACT_LIST {
   ACCOUNT = 'Account',
 }
@@ -25,10 +17,6 @@ export const loadContract = (name: CONTRACT_LIST): CompiledContract => {
 }
 
 export const accountContractLoader = () => loadContract(CONTRACT_LIST.ACCOUNT)
-
-export const calculateAddress = (salt: number, publicKey: string): string => {
-  return hash.calculateContractAddressFromHash(salt, CONTRACT_HASH, [publicKey], 0)
-}
 
 // use bignumber libraries to assert addresses are equal
 // handles prepending 0s
