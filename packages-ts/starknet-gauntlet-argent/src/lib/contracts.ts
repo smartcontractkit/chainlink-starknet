c1cab20f716463f81bff0e93f85cfc83b4e8bf31
cfal <cfal@users.noreply.github.com>
2023-04-30 02:21:04 +0900

    packages-ts/starknet-gauntlet-argent/lib/contracts.ts: update contract paths

--- packages-ts/starknet-gauntlet-argent/src/lib/contracts.ts
+++ packages-ts/starknet-gauntlet-argent/src/lib/contracts.ts
@@ -5,10 +5,21 @@ export enum CONTRACT_LIST {
   ACCOUNT = 'argent_account',
 }
 
-export const loadContract = (name: CONTRACT_LIST): CompiledContract => {
-  return json.parse(
-    fs.readFileSync(`${__dirname}/../../artifacts/abi/${name}.json`).toString('ascii'),
-  )
+export const loadContract = (name: CONTRACT_LIST) => {
+  return {
+    contract: json.parse(
+      fs.readFileSync(
+        `${__dirname}/../../../../contracts/target/release/chainlink_${name}.sierra.json`,
+        'utf-8',
+      ),
+    ),
+    casm: json.parse(
+      fs.readFileSync(
+        `${__dirname}/../../../../contracts/target/release/chainlink_${name}.casm.json`,
+        'utf-8',
+      ),
+    ),
+  }
 }
 
 export const accountContractLoader = () => loadContract(CONTRACT_LIST.ACCOUNT)
