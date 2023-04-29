4d3e915056031e6c0da5b7d7030e94ba372fcccd
cfal <cfal@users.noreply.github.com>
2023-04-30 02:21:37 +0900

    packages-ts/starknet-gauntlet-multisig: update contract paths

--- packages-ts/starknet-gauntlet-multisig/src/lib/contracts.ts
+++ packages-ts/starknet-gauntlet-multisig/src/lib/contracts.ts
@@ -5,14 +5,21 @@ export enum CONTRACT_LIST {
   MULTISIG = 'Multisig',
 }
 
-export const loadContract = (name: CONTRACT_LIST): CompiledContract => {
-  return json.parse(
-    fs
-      .readFileSync(
-        `${__dirname}/../../../../node_modules/starsign-multisig/starknet-artifacts/contracts/${name}.cairo/${name}.json`,
-      )
-      .toString('ascii'),
-  )
+export const loadContract = (name: CONTRACT_LIST) => {
+  return {
+    contract: json.parse(
+      fs.readFileSync(
+        `${__dirname}/../../../../contracts/target/release/chainlink_${name}.sierra.json`,
+        'utf-8',
+      ),
+    ),
+    casm: json.parse(
+      fs.readFileSync(
+        `${__dirname}/../../../../contracts/target/release/chainlink_${name}.casm.json`,
+        'utf-8',
+      ),
+    ),
+  }
 }
 
 export const contractLoader = () => loadContract(CONTRACT_LIST.MULTISIG)
