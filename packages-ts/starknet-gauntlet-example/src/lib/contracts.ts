240d8fbcd0aa2ed5458a0c8ba1ef381725be12c3
cfal <cfal@users.noreply.github.com>
2023-04-30 02:21:26 +0900

    packages-ts/starknet-gauntlet-example/lib/contracts.ts: update contract paths

--- packages-ts/starknet-gauntlet-example/src/lib/contracts.ts
+++ packages-ts/starknet-gauntlet-example/src/lib/contracts.ts
@@ -1,14 +1,25 @@
 import fs from 'fs'
-import { CompiledContract, json } from 'starknet'
+import { json } from 'starknet'
 
 export enum CONTRACT_LIST {
   EXAMPLE = 'example',
 }
 
-export const loadContract = (name: CONTRACT_LIST): CompiledContract => {
-  return json.parse(
-    fs.readFileSync(`${__dirname}/../../contract_artifacts/abi/${name}.json`).toString('ascii'),
-  )
+export const loadContract = (name: CONTRACT_LIST) => {
+  return {
+    contract: json.parse(
+      fs.readFileSync(
+        `${__dirname}/../../../../contracts/target/release/chainlink_${name}.sierra.json`,
+        'utf-8',
+      ),
+    ),
+    casm: json.parse(
+      fs.readFileSync(
+        `${__dirname}/../../../../contracts/target/release/chainlink_${name}.casm.json`,
+        'utf-8',
+      ),
+    ),
+  }
 }
 
 export const tokenContractLoader = () => loadContract(CONTRACT_LIST.EXAMPLE)
