f9dac999b2b166c292c1a2e67e785a8963d03c58
cfal <cfal@users.noreply.github.com>
2023-05-09 16:32:20 +0900

    packages-ts/integration-multisig/test/Multisig.test.ts: update for cairo 1.0

--- packages-ts/integration-multisig/test/Multisig.test.ts
+++ packages-ts/integration-multisig/test/Multisig.test.ts
@@ -2,7 +2,7 @@ import { expect } from 'chai'
 import { starknet } from 'hardhat'
 import { StarknetContract, Account } from 'hardhat/types/runtime'
 import { num, hash } from 'starknet'
-import { account } from '@chainlink/starknet'
+import { account, expectSuccessOrDeclared } from '@chainlink/starknet'
 
 describe('Multisig integration tests', function () {
   this.timeout(300_000)
@@ -32,8 +32,8 @@ describe('Multisig integration tests', function () {
   })
 
   it('Deploy contract', async () => {
-    let multisigFactory = await starknet.getContractFactory('Multisig')
-    await account1.declare(multisigFactory)
+    let multisigFactory = await starknet.getContractFactory('multisig')
+    await expectSuccessOrDeclared(account1.declare(multisigFactory, { maxFee: 1e20 }))
 
     multisig = await account1.deploy(multisigFactory, {
       signers: [
@@ -82,7 +82,7 @@ describe('Multisig integration tests', function () {
 
     {
       const res = await multisig.call('get_threshold')
-      expect(res.threshold).to.equal(newThreshold)
+      expect(res.response).to.equal(newThreshold)
     }
   })
 })
