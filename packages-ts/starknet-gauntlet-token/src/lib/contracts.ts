c64849e6d582155b463efecec9bbdf94e24b8df4
cfal <cfal@users.noreply.github.com>
2023-05-11 15:03:09 +0900

    packages-ts: use loadContract from starknet-gauntlet

--- packages-ts/starknet-gauntlet-token/src/lib/contracts.ts
+++ packages-ts/starknet-gauntlet-token/src/lib/contracts.ts
@@ -1,31 +1,7 @@
-import fs from 'fs'
-import { json } from 'starknet'
+import { loadContract } from '@chainlink/starknet-gauntlet'
 
 export enum CONTRACT_LIST {
-  TOKEN = 'token',
+  TOKEN = 'LinkToken',
 }
 
-// todo: remove when stable contract artifacts release available
-const CONTRACT_NAME_TO_ARTIFACT_NAME = {
-  [CONTRACT_LIST.TOKEN]: 'LinkToken',
-}
-
-export const loadTokenContract = (name: CONTRACT_LIST) => {
-  const artifactName = CONTRACT_NAME_TO_ARTIFACT_NAME[name]
-  return {
-    contract: json.parse(
-      fs.readFileSync(
-        `${__dirname}/../../../../contracts/target/release/chainlink_${artifactName}.sierra.json`,
-        'utf-8',
-      ),
-    ),
-    casm: json.parse(
-      fs.readFileSync(
-        `${__dirname}/../../../../contracts/target/release/chainlink_${artifactName}.casm.json`,
-        'utf-8',
-      ),
-    ),
-  }
-}
-
-export const tokenContractLoader = () => loadTokenContract(CONTRACT_LIST.TOKEN)
+export const tokenContractLoader = () => loadContract(CONTRACT_LIST.TOKEN)
