058fdc2c8153dd030bf39743c210682fc0426312
Calvin Wang <calvin.wang@smartcontract.com>
2023-05-31 18:49:34 +1000

    use ctx.rdd and add manual cli args support

--- packages-ts/starknet-gauntlet-ocr2/src/commands/ocr2/setPayees.ts
+++ packages-ts/starknet-gauntlet-ocr2/src/commands/ocr2/setPayees.ts
@@ -18,18 +18,26 @@ type ContractInput = [payees: PayeeConfig[]]
 
 const makeUserInput = async (flags, args): Promise<UserInput> => {
   if (flags.input) return flags.input as UserInput
-  const rdd = getRDD(flags.rdd)
-  const contractAddr = args[0]
-  const aggregator = rdd[CONTRACT_TYPES.AGGREGATOR][contractAddr]
-  if (!aggregator || !aggregator.payees)
-    throw new Error(`Payees not defined in RDD for contract ${contractAddr}`)
-  return aggregator.payees
+  if (args.length % 2 != 0) throw new Error('Invalid number of arguments for payee config')
+  let userInput: UserInput = []
+  for (let i = 0; i < args.length; i += 2) {
+    userInput.push({
+      transmitter: args[i],
+      payee: args[i + 1],
+    })
+  }
+  return userInput
 }
 
 const makeContractInput = async (
   input: UserInput,
   ctx: ExecutionContext,
 ): Promise<ContractInput> => {
+  if (ctx.rdd) {
+    const aggregator = ctx.rdd[CONTRACT_TYPES.AGGREGATOR][ctx.contractAddress]
+    input = aggregator.payees
+  }
+
   return input.map((payeeConfig) => [
     {
       transmitter: payeeConfig.transmitter,
@@ -45,7 +53,7 @@ const commandConfig: ExecuteCommandConfig<UserInput, ContractInput> = {
   ux: {
     description: 'Set payees of OCR2 contract',
     examples: [
-      `yarn gauntlet ocr2:set_payees --network=<NETWORK> --rdd=<RDD_PATH> <CONTRACT_ADDRESS>`,
+      `yarn gauntlet ocr2:set_payees --network=<NETWORK> --rdd=<RDD_PATH> <CONTRACT_ADDRESS> <TRANSMITTER_1> <PAYEE_1> <TRANSMITTER_2> <PAYEE_2> ...`,
     ],
   },
   validations: [],
