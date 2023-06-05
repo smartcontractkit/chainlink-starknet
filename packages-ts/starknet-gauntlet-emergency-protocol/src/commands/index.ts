4c4e6ed207c74a39d1068893a0f8b10cb5dc0f69
Sergei Drugalev <sergei.drugalev@smartcontract.com>
2023-06-06 00:50:41 +0600

    Folder renamings & cleanup

--- packages-ts/starknet-gauntlet-emergency-protocol/src/commands/index.ts
+++ packages-ts/starknet-gauntlet-emergency-protocol/src/commands/index.ts
@@ -2,8 +2,10 @@ import {
   executionCommands as UptimeFeedCommands,
   inspectionCommands as UptimeInspectionCommands,
 } from './sequencerUptimeFeed'
-import StarknetValidatorCommands from './starknetValidator'
+import StarknetValidatorCommands from './L1StarknetValidator'
+import L1AccessController from './L1AccessController'
+import L1GasPriceFeed from './L1GasPriceFeed'
 
-export const L1Commands = [...StarknetValidatorCommands]
+export const L1Commands = [...StarknetValidatorCommands, ...L1AccessController, ...L1GasPriceFeed]
 export const L2Commands = [...UptimeFeedCommands]
 export const L2InspectionCommands = [...UptimeInspectionCommands]
