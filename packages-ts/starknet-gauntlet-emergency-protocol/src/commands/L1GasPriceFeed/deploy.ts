38d270e8795f8f67104db9bf18cf0db76635d08f
Sergei Drugalev <sergei.drugalev@smartcontract.com>
2023-06-06 00:49:51 +0600

    L1 MockAggregator deploy and set_answer gauntlet commands

---
+++ packages-ts/starknet-gauntlet-emergency-protocol/src/commands/L1GasPriceFeed/deploy.ts
@@ -0,0 +1,29 @@
+import { EVMExecuteCommandConfig, makeEVMExecuteCommand } from '@chainlink/evm-gauntlet'
+import { CATEGORIES } from '../../lib/categories'
+import { L1MockAggregatorLoader, CONTRACT_LIST } from '../../lib/contracts'
+
+type UserInput = {}
+
+type ContractInput = []
+
+const makeUserInput = async (flags, args, env): Promise<UserInput> => ({})
+
+const makeContractInput = async (input: UserInput): Promise<ContractInput> => {
+  return []
+}
+
+const commandConfig: EVMExecuteCommandConfig<UserInput, ContractInput> = {
+  contractId: CONTRACT_LIST.L1_MOCK_AGGREGATOR,
+  category: CATEGORIES.L1_MOCK_AGGREGATOR,
+  action: 'deploy',
+  ux: {
+    description: 'Deploys an Mock Aggregator as an L1GasPriceFeed',
+    examples: [`${CATEGORIES.L1_MOCK_AGGREGATOR}:deploy --network=<NETWORK>`],
+  },
+  makeUserInput,
+  makeContractInput,
+  validations: [],
+  loadContract: L1MockAggregatorLoader,
+}
+
+export default makeEVMExecuteCommand(commandConfig)
