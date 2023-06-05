dd0c65ae3b66c48d8a311223b6c63b714bc49bae
Sergei Drugalev <sergei.drugalev@smartcontract.com>
2023-06-06 00:49:22 +0600

    L1 AccessController gauntlet deploy command

---
+++ packages-ts/starknet-gauntlet-emergency-protocol/src/commands/L1AccessController/deploy.ts
@@ -0,0 +1,29 @@
+import { EVMExecuteCommandConfig, makeEVMExecuteCommand } from '@chainlink/evm-gauntlet'
+import { CATEGORIES } from '../../lib/categories'
+import { L1AccessControllerContractLoader, CONTRACT_LIST } from '../../lib/contracts'
+
+type UserInput = {}
+
+type ContractInput = []
+
+const makeUserInput = async (flags, args, env): Promise<UserInput> => ({})
+
+const makeContractInput = async (input: UserInput): Promise<ContractInput> => {
+  return []
+}
+
+const commandConfig: EVMExecuteCommandConfig<UserInput, ContractInput> = {
+  contractId: CONTRACT_LIST.L1_ACCESS_CONTROLLER,
+  category: CATEGORIES.L1_ACCESS_CONTROLLER,
+  action: 'deploy',
+  ux: {
+    description: 'Deploys an Access Controller Contract',
+    examples: [`${CATEGORIES.L1_ACCESS_CONTROLLER}:deploy --network=<NETWORK>`],
+  },
+  makeUserInput,
+  makeContractInput,
+  validations: [],
+  loadContract: L1AccessControllerContractLoader,
+}
+
+export default makeEVMExecuteCommand(commandConfig)
