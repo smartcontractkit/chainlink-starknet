52639fc52f1c968713576c4c3e9cf0b749ea27a5
Augustus Chang <augustus.chang@smartcontract.com>
2023-06-06 14:43:44 -0400

    correct types

--- relayer/pkg/chainlink/ocr2/medianreport/report.go
+++ relayer/pkg/chainlink/ocr2/medianreport/report.go
@@ -119,7 +119,7 @@ func (c ReportCodec) MedianFromReport(report types.Report) (*big.Int, error) {
 	for i := 0; i < n; i++ {
 		start := prefixSizeBytes + observationSizeBytes*i
 		end := start + observationSizeBytes
-		o := starknet.FeltToUnSignedBig(&caigotypes.BytesToFelt(report[start:end]))
+		o := starknet.FeltToUnsignedBig(caigotypes.BytesToFelt(report[start:end]))
 		oo = append(oo, o)
 	}
 
