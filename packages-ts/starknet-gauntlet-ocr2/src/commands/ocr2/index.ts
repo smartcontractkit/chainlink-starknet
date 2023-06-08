ec8ed49aa17c4cadd99a71e1a7e40b50846b883c
Augustus Chang <augustus.chang@smartcontract.com>
2023-06-08 16:17:35 -0400

    initial

--- packages-ts/starknet-gauntlet-ocr2/src/commands/ocr2/index.ts
+++ packages-ts/starknet-gauntlet-ocr2/src/commands/ocr2/index.ts
@@ -1,9 +1,17 @@
 import Deploy from './deploy'
+import Upgrade from './upgrade'
 import inspect from './inspection/inspect'
 import SetBilling from './setBilling'
 import SetConfig from './setConfig'
 import AddAccess from './addAccess'
 import DisableAccessCheck from './disableAccessCheck'
 
-export const executeCommands = [Deploy, AddAccess, DisableAccessCheck, SetBilling, SetConfig]
+export const executeCommands = [
+  Deploy,
+  Upgrade,
+  AddAccess,
+  DisableAccessCheck,
+  SetBilling,
+  SetConfig,
+]
 export const inspectionCommands = [inspect]
