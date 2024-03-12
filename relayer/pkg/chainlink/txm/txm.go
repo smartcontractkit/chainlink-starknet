a9ccf97e120255441199b47a3733dcc5a1f13799
Blaž Hrastnik <blaz@mxxn.io>
2024-03-12 14:03:51 +0900

    minor: Fix broken lint

--- relayer/pkg/chainlink/txm/txm.go
+++ relayer/pkg/chainlink/txm/txm.go
@@ -200,8 +200,9 @@ func (txm *starktxm) broadcast(ctx context.Context, publicKey *felt.Felt, accoun
 	feeEstimate, err := account.EstimateFee(ctx, []starknetrpc.BroadcastTxn{tx}, simFlags, starknetrpc.BlockID{Tag: "latest"})
 	if err != nil {
 		var data any
-		if err, ok := err.(ethrpc.DataError); ok {
-			data = err.ErrorData()
+		var dataErr ethrpc.DataError
+		if errors.As(err, &dataErr) {
+			data = dataErr.ErrorData()
 		}
 		txm.lggr.Errorw("failed to estimate fee", "error", err, "data", data)
 		return txhash, fmt.Errorf("failed to estimate fee: %T %+w", err, err)
@@ -252,8 +253,9 @@ func (txm *starktxm) broadcast(ctx context.Context, publicKey *felt.Felt, accoun
 	if err != nil {
 		// TODO: handle initial broadcast errors - what kind of errors occur?
 		var data any
-		if err, ok := err.(ethrpc.DataError); ok {
-			data = err.ErrorData()
+		var dataErr ethrpc.DataError
+		if errors.As(err, &dataErr) {
+			data = dataErr.ErrorData()
 		}
 		txm.lggr.Errorw("failed to invoke tx", "error", err, "data", data)
 		return txhash, fmt.Errorf("failed to invoke tx: %+w", err)
