b37f20173c8c5143401edfbcc3c5f286d09558e3
Augustus Chang <augustus.chang@smartcontract.com>
2024-03-14 16:12:03 -0400

    add one method and one test

---
+++ relayer/pkg/starknet/chain_client.go
@@ -0,0 +1,60 @@
+package starknet
+
+import (
+	"context"
+
+	"github.com/NethermindEth/juno/core/felt"
+	starknetrpc "github.com/NethermindEth/starknet.go/rpc"
+	gethrpc "github.com/ethereum/go-ethereum/rpc"
+	"github.com/pkg/errors"
+)
+
+// type alias for readibility
+type FinalizedBlock = starknetrpc.Block
+
+// used to create batch requests
+type StarknetBatchBuilder interface {
+	RequestBlockByHash(ctx context.Context, h *felt.Felt) (*StarknetBatchBuilder, error)
+	RequestBlockByNumber(ctx context.Context, id uint64) (*StarknetBatchBuilder, error)
+	RequestLatestPendingBlock(ctx context.Context) (*StarknetBatchBuilder, error)
+	LatestBlockHashAndNumber(ctx context.Context) (*StarknetBatchBuilder, error)
+	EventsByFilter(ctx context.Context, f starknetrpc.EventFilter) (*StarknetBatchBuilder, error)
+	TxReceiptByHash(ctx context.Context, h *felt.Felt) (*StarknetBatchBuilder, error)
+}
+
+type StarknetChainClient interface {
+	// only finalized blocks have a block hashes
+	BlockByHash(ctx context.Context, h *felt.Felt) (FinalizedBlock, error)
+	// only finalized blocks have numbers
+	BlockByNumber(ctx context.Context, id uint64) (FinalizedBlock, error)
+	// only way to get the latest pending block (only 1 pending block exists at a time)
+	LatestPendingBlock(ctx context.Context) (starknetrpc.PendingBlock, error)
+	// returns block number and block has of latest finalized block
+	LatestBlockHashAndNumber(ctx context.Context) (starknetrpc.BlockHashAndNumberOutput, error)
+	// get block logs, event logs, etc.
+	EventsByFilter(ctx context.Context, f starknetrpc.EventFilter) ([]starknetrpc.EmittedEvent, error)
+	TxReceiptByHash(ctx context.Context, h *felt.Felt) (starknetrpc.TransactionReceipt, error)
+	Batch(builder *StarknetBatchBuilder) ([]gethrpc.BatchElem, error)
+}
+
+func (c *Client) BlockByHash(ctx context.Context, h *felt.Felt) (FinalizedBlock, error) {
+	if c.defaultTimeout != 0 {
+		var cancel context.CancelFunc
+		ctx, cancel = context.WithTimeout(ctx, c.defaultTimeout)
+		defer cancel()
+	}
+
+	block, err := c.Provider.BlockWithTxs(ctx, starknetrpc.BlockID{Hash: h})
+
+	if err != nil {
+		return FinalizedBlock{}, errors.Wrap(err, "error in BlockByHash")
+	}
+
+	finalizedBlock, ok := block.(*FinalizedBlock)
+
+	if !ok {
+		return FinalizedBlock{}, errors.Errorf("expected type Finalized block but found: %T", block)
+	}
+
+	return *finalizedBlock, nil
+}
---
+++ relayer/pkg/starknet/chain_client_test.go
@@ -0,0 +1,111 @@
+package starknet
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/NethermindEth/juno/core/felt"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/smartcontractkit/chainlink-common/pkg/logger"
+)
+
+var (
+	myChainID = "SN_SEPOLIA"
+	myTimeout = 10 * time.Second
+)
+
+func TestChainClient(t *testing.T) {
+	mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		req, _ := io.ReadAll(r.Body)
+		fmt.Println(r.RequestURI, r.URL, string(req))
+
+		var out []byte
+
+		type Call struct {
+			Method string            `json:"method"`
+			Params []json.RawMessage `json:"params"`
+		}
+
+		call := Call{}
+		require.NoError(t, json.Unmarshal(req, &call))
+
+		switch call.Method {
+		case "starknet_getBlockWithTxs":
+			out = []byte(fmt.Sprintf(` 
+			{
+        "result": {
+            "status": "ACCEPTED_ON_L2",
+            "block_hash": "0x725407fcc3bd43e50884f50f1e0ef32aa9f814af3da475411934a7dbd4b41a",
+            "parent_hash": "0x5ac8b4099a26e9331a015f8437feadf56fa7fb447e8183aa1bdb3bf541a2cbb",
+            "block_number": 48719,
+            "new_root": "0x624f0f3cf2fbbd5951b0d90e4e1fc858f3d77cf34303781fcf3e4dc3afaf666",
+            "timestamp": 1710445796,
+            "sequencer_address": "0x1176a1bd84444c89232ec27754698e5d2e7e1a7f1539f12027f28b23ec9f3d8",
+            "l1_gas_price": {
+                "price_in_fri": "0x1d1a94a20000",
+                "price_in_wei": "0x4a817c800"
+            },
+            "starknet_version": "0.13.1",
+            "transactions": [
+                {
+                    "transaction_hash": "0x27a9a9bc927efc37658acfb9c27b1fc56e7cfcf7a30db1ecdd9820bb2dddf0c",
+                    "type": "INVOKE",
+                    "version": "0x1",
+                    "nonce": "0x62b",
+                    "max_fee": "0x271963aac565a",
+                    "sender_address": "0x42db30408353b25c5a0b3dd798bfe98eba08956786374e961cc5dbb9811ec6e",
+                    "signature": [
+                        "0x66f70855f35096c6aea45be365617bc70fca65808bb85332dd3c2f6f4a86071",
+                        "0x43c09cab9be65cdc59b29b8018c201c1eb42d51529cb9b5dd64859652bf827f"
+                    ],
+                    "calldata": [
+                        "0x1",
+                        "0x517567ac7026ce129c950e6e113e437aa3c83716cd61481c6bb8c5057e6923e",
+                        "0xcaffbd1bd76bd7f24a3fa1d69d1b2588a86d1f9d2359b13f6a84b7e1cbd126",
+                        "0xa",
+                        "0x53616d706c654465706f7369745374617274",
+                        "0x8",
+                        "0x4",
+                        "0x18343d00000001",
+                        "0x1",
+                        "0x5",
+                        "0x469",
+                        "0x2",
+                        "0x1",
+                        "0x4eb"
+                    ]
+                }]
+					}
+				}`))
+		case "starknet_blockNumber":
+			out = []byte(`{"result": 1}`)
+		default:
+			require.False(t, true, "unsupported RPC method %s", call.Method)
+		}
+		_, err := w.Write(out)
+		require.NoError(t, err)
+	}))
+	defer mockServer.Close()
+
+	lggr := logger.Test(t)
+	client, err := NewClient(chainID, mockServer.URL, lggr, &myTimeout)
+	require.NoError(t, err)
+	assert.Equal(t, myTimeout, client.defaultTimeout)
+
+	t.Run("get BlockByHash", func(t *testing.T) {
+		blockHash, err := new(felt.Felt).SetString("0x725407fcc3bd43e50884f50f1e0ef32aa9f814af3da475411934a7dbd4b41a")
+		require.NoError(t, err)
+
+		block, err := client.BlockByHash(context.TODO(), blockHash)
+		require.NoError(t, err)
+		assert.Equal(t, blockHash, block.BlockHash)
+	})
+}
