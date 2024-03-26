90cdc1641730248c10ba116ff50ee082665bd862
Augustus <14297860+augustbleeds@users.noreply.github.com>
2024-03-25 22:12:00 -0700

    add upgrade cmd to multisig (#372)
    * add upgrade cmd to multisig
    
    * format

---
+++ packages-ts/starknet-gauntlet-multisig/src/commands/multisig/upgrade.ts
@@ -0,0 +1,7 @@
+import { makeExecuteCommand, upgradeCommandConfig } from '@chainlink/starknet-gauntlet'
+import { CATEGORIES } from '../../lib/categories'
+import { contractLoader } from '../../lib/contracts'
+
+export default makeExecuteCommand(
+  upgradeCommandConfig(CATEGORIES.MULTISIG, CATEGORIES.MULTISIG, contractLoader),
+)
