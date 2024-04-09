14a0774f09cd4d0b8de821b31ef840573d8c94c0
Blaž Hrastnik <blaz@mxxn.io>
2024-04-09 16:19:46 +0900

    Remove gauntlet exmaple commands (we no longer use it for examples/)

--- packages-ts/starknet-gauntlet-ocr2/src/commands/example/index.ts
+++
@@ -1,6 +0,0 @@
-import Deploy from './deploy'
-import Declare from './declare'
-import Inspect from './inspect'
-
-export const executeCommands = [Deploy, Declare]
-export const inspectionCommands = [Inspect]
