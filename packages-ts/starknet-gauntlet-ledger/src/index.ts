ea3d904d04d5e5842d6e7929a1ac950b6b59f582
Blaž Hrastnik <blaz@mxxn.io>
2024-04-10 15:08:52 +0900

    starknet-gauntlet-ledger: Clean up getPubKey()

--- packages-ts/starknet-gauntlet-ledger/src/index.ts
+++ packages-ts/starknet-gauntlet-ledger/src/index.ts
@@ -1,6 +1,6 @@
 import { IStarknetWallet, Env } from '@chainlink/starknet-gauntlet'
 import { encode, Signature, ec, Signer } from 'starknet'
-import { Stark, LedgerError } from '@ledgerhq/hw-app-starknet'
+import { Stark as LedgerClient, LedgerError } from '@ledgerhq/hw-app-starknet'
 
 // EIP-2645 path
 //  2645 - EIP number
@@ -10,15 +10,14 @@ export const DEFAULT_LEDGER_PATH = "m/2645'/579218131'/894929996'/0'"
 export const LEDGER_PATH_REGEX = /^\s*m\s*\/\s*2645\s*\'\s*\/\s*579218131\s*\'\s*\/\s*(\d+)\s*\'\s*\/\s*(\d+)\s*\'$/
 
 class LedgerSigner extends Signer {
-  client: Stark
+  client: LedgerClient
   path: string
   publicKey: string
 
-  private constructor(client: Stark, path: string, publicKey: string) {
+  private constructor(client: LedgerClient, path: string) {
     super()
     this.client = client
     this.path = path
-    this.publicKey = publicKey
   }
 
   static create = async (path?: string): Promise<LedgerSigner> => {
@@ -37,20 +36,24 @@ class LedgerSigner extends Signer {
     // package is only allowed to be imported once
     const Transport = (await import('@ledgerhq/hw-transport-node-hid')).default
     const transport = await Transport.create()
-    const ledgerConnector = new Stark(transport)
+    const ledgerConnector = new LedgerClient(transport)
 
-    const response = await ledgerConnector.getPubKey(path)
-    if (response.returnCode != LedgerError.NoErrors) {
-      throw new Error(`Unable to get public key: ${response.errorMessage}. Is Ledger app active?`)
-    }
-
-    const publicKey = encode.addHexPrefix(encode.buf2hex(response.publicKey).slice(2, 2 + 64))
-    const signer = new LedgerSigner(ledgerConnector, path, publicKey)
+    const signer = new LedgerSigner(ledgerConnector, path)
 
     return signer
   }
 
   public async getPubKey(): Promise<string> {
+    // memoize to avoid redundant calls
+    if (this.publicKey) return this.publicKey
+
+    const response = await this.client.getPubKey(this.path)
+    if (response.returnCode != LedgerError.NoErrors) {
+      throw new Error(`Unable to get public key: ${response.errorMessage}. Is Ledger app active?`)
+    }
+
+    this.publicKey = encode.addHexPrefix(encode.buf2hex(response.publicKey).slice(2, 2 + 64))
+
     return this.publicKey
   }
 
@@ -59,6 +62,8 @@ class LedgerSigner extends Signer {
     if (response.returnCode != LedgerError.NoErrors) {
       throw new Error(`Unable to sign the message: ${response.errorMessage}`)
     }
+
+    // TODO: console log the hash so user can verify on ledger
 
     return new ec.starkCurve.Signature(
       BigInt(encode.addHexPrefix(encode.buf2hex(response.r))),
