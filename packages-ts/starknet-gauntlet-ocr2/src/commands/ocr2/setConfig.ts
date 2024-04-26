f2592705329d0d6a1781cecfb8ba21ed702f7862
Augustus Chang <augustus.chang@smartcontract.com>
2024-04-26 11:26:43 -0400

    destructure correctly

--- packages-ts/starknet-gauntlet-ocr2/src/commands/ocr2/setConfig.ts
+++ packages-ts/starknet-gauntlet-ocr2/src/commands/ocr2/setConfig.ts
@@ -185,13 +185,14 @@ const afterExecute: AfterExecute<SetConfigInput, ContractInput> = (context, inpu
 
     // write lastConfigDigest back to RDD
     const configDigest = `0x${(event.latest_config_digest as bigint).toString(16)}`
-    deps.logger.info(`ℹ️  lastConfigDigest to save in RDD: ${configDigest}`)
+    deps.logger.info(`ℹ️ lastConfigDigest to save in RDD: ${configDigest}`)
     if (context.flags.rdd) {
       deps.logger.info(`ℹ️ RDD file ${context.flags.rdd} found! Will automatically update lastConfigDigest for you`)
       const rdd = getRDD(context.flags.rdd)
-      const newRdd = { ...rdd, ...{ 'lastConfigDigest': configDigest } }
+      const newConfig = { ...rdd.config, ...{ 'lastConfigDigest': configDigest } }
+      const newRdd = { ...rdd, ...{ config: newConfig } }
       fs.writeFileSync(context.flags.rdd, JSON.stringify(newRdd, null, 2))
-      deps.logger.info(`✅ RDD file ${context.flags.rdd} updated. Please reformat (run ./bin/generate and ./bin/degenerate) as needed`)
+      deps.logger.info(`✅ RDD file ${context.flags.rdd} updated. Please reformat RDD (run ./bin/generate and ./bin/degenerate) as needed`)
     } else {
       deps.logger.info(`❗❗ no rdd file input, you must manually update lastConfigDigest in rdd yourself`)
     }
