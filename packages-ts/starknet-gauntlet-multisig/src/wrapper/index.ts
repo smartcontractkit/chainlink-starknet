4fc93167825dcc3f7f0c1a5510fae15911f4d169
Augustus Chang <augustus.chang@smartcontract.com>
2024-06-10 18:36:22 -0400

    fix

--- packages-ts/starknet-gauntlet-multisig/src/wrapper/index.ts
+++ packages-ts/starknet-gauntlet-multisig/src/wrapper/index.ts
@@ -42,258 +42,260 @@ export const wrapCommand = <UI, CI>(
 ): UnregisteredCommand<UserInput, ContractInput> => (
   deps: Dependencies,
 ): CommandCtor<ExecuteCommandInstance<UserInput, ContractInput>> => {
-  const id = `${registeredCommand.id}:multisig`
+    const id = `${registeredCommand.id}:multisig`
 
-  const msigCommand: CommandCtor<ExecuteCommandInstance<
-    UserInput,
-    ContractInput
-  >> = class MsigCommand
-    extends WriteCommand<TransactionResponse>
-    implements ExecuteCommandInstance<UserInput, ContractInput> {
-    wallet: IStarknetWallet
-    provider: IStarknetProvider
-    contractAddress: string
-    account: string
-    executionContext: ExecutionContext
-    contract: CompiledContract
-    compiledContractHash?: string
+    const msigCommand: CommandCtor<ExecuteCommandInstance<
+      UserInput,
+      ContractInput
+    >> = class MsigCommand
+        extends WriteCommand<TransactionResponse>
+        implements ExecuteCommandInstance<UserInput, ContractInput> {
+        wallet: IStarknetWallet
+        provider: IStarknetProvider
+        contractAddress: string
+        account: string
+        executionContext: ExecutionContext
+        contract: CompiledContract
+        compiledContractHash?: string
 
-    input: Input<UserInput, ContractInput>
+        input: Input<UserInput, ContractInput>
 
-    command: ExecuteCommandInstance<UI, CI>
-    multisigAddress: string
-    initialState: State
+        command: ExecuteCommandInstance<UI, CI>
+        multisigAddress: string
+        initialState: State
 
-    static id = id
-    static category = registeredCommand.category
-    static examples = registeredCommand.examples
+        static id = id
+        static category = registeredCommand.category
+        static examples = registeredCommand.examples
 
-    constructor(flags, args) {
-      super(flags, args)
-    }
+        constructor(flags, args) {
+          super(flags, args)
+        }
 
-    static create = async (flags, args) => {
-      const c = new MsigCommand(flags, args)
+        static create = async (flags, args) => {
+          const c = new MsigCommand(flags, args)
 
-      const env = await deps.makeEnv(flags)
+          c.contractAddress = args[0]
+          c.command = await registeredCommand.create(flags, [c.contractAddress])
 
-      c.wallet = await deps.makeWallet(env)
-      c.provider = deps.makeProvider(env.providerUrl, c.wallet)
-      c.contractAddress = args[0]
-      c.account = env.account
-      c.multisigAddress = env.multisig
-      const loadResult = contractLoader()
-      c.contract = loadResult.contract
-      if (loadResult.casm) {
-        c.compiledContractHash = hash.computeCompiledClassHash(loadResult.casm)
-      }
+          const env = await deps.makeEnv(flags)
 
-      c.executionContext = {
-        provider: c.provider,
-        wallet: c.wallet,
-        category: registeredCommand.id,
-        action: 'multisig',
-        id,
-        contractAddress: c.contractAddress,
-        flags: flags,
-        contract: new Contract(c.contract.abi, c.multisigAddress, c.provider.provider),
-      }
+          const { wallet, provider, account } = c.command
+          c.wallet = wallet
+          c.provider = provider
+          c.account = account
 
-      c.input = {
-        user: flags.input || {
-          proposalId: Number(flags.proposalId || flags.multisigProposal),
-        },
-        contract: {},
-      }
+          c.account = env.account
+          c.multisigAddress = env.multisig
+          const loadResult = contractLoader()
+          c.contract = loadResult.contract
+          if (loadResult.casm) {
+            c.compiledContractHash = hash.computeCompiledClassHash(loadResult.casm)
+          }
 
-      c.command = await registeredCommand.create(flags, [c.contractAddress])
+          c.executionContext = {
+            provider: c.provider,
+            wallet: c.wallet,
+            category: registeredCommand.id,
+            action: 'multisig',
+            id,
+            contractAddress: c.contractAddress,
+            flags: flags,
+            contract: new Contract(c.contract.abi, c.multisigAddress, c.provider.provider),
+          }
 
-      c.initialState = await c.fetchMultisigState(c.multisigAddress, c.input.user.proposalId)
+          c.input = {
+            user: flags.input || {
+              proposalId: Number(flags.proposalId || flags.multisigProposal),
+            },
+            contract: {},
+          }
 
-      return c
-    }
+          c.initialState = await c.fetchMultisigState(c.multisigAddress, c.input.user.proposalId)
 
-    fetchMultisigState = async (address: string, proposalId?: number): Promise<State> => {
-      const [signers, threshold] = await Promise.all(
-        ['get_signers', 'get_threshold'].map((func) => {
-          return this.executionContext.contract[func]()
-        }),
-      )
-      const multisig = {
-        address,
-        threshold,
-        signers: signers.map((o) => num.toHex(o)),
-      }
+          return c
+        }
 
-      if (isNaN(proposalId)) return { multisig }
-      const { 0: tx, 1: calldata } = await this.executionContext.contract.get_transaction(
-        proposalId,
-      )
-      return {
-        multisig,
-        proposal: {
-          id: proposalId,
-          confirmations: tx.confirmations,
-          data: {
-            contractAddress: addAddressPadding(tx.to),
-            entrypoint: encode.addHexPrefix(num.toHex(num.toBigInt(tx.function_selector))),
-            calldata: calldata.map((v) => num.toBigInt(v).toString()),
-          },
-          nextAction:
-            Number(num.toBigInt(tx.executed)) !== 0
-              ? Action.NONE
-              : tx.confirmations >= multisig.threshold
-              ? Action.EXECUTE
-              : Action.APPROVE,
-        },
-      }
-    }
+        fetchMultisigState = async (address: string, proposalId?: number): Promise<State> => {
+          const [signers, threshold] = await Promise.all(
+            ['get_signers', 'get_threshold'].map((func) => {
+              return this.executionContext.contract[func]()
+            }),
+          )
+          const multisig = {
+            address,
+            threshold,
+            signers: signers.map((o) => num.toHex(o)),
+          }
 
-    isSameProposal = (local: Call, onchain: Call): boolean => {
-      const localAddress = validateAndParseAddress(local.contractAddress)
-      const onchainAddress = validateAndParseAddress(onchain.contractAddress)
-      if (localAddress !== onchainAddress) return false
-      if (hash.getSelectorFromName(local.entrypoint) !== onchain.entrypoint) return false
-      if (!isDeepEqual(local.calldata, onchain.calldata)) return false
-      return true
-    }
+          if (isNaN(proposalId)) return { multisig }
+          const { 0: tx, 1: calldata } = await this.executionContext.contract.get_transaction(
+            proposalId,
+          )
+          return {
+            multisig,
+            proposal: {
+              id: proposalId,
+              confirmations: tx.confirmations,
+              data: {
+                contractAddress: addAddressPadding(tx.to),
+                entrypoint: encode.addHexPrefix(num.toHex(num.toBigInt(tx.function_selector))),
+                calldata: calldata.map((v) => num.toBigInt(v).toString()),
+              },
+              nextAction:
+                Number(num.toBigInt(tx.executed)) !== 0
+                  ? Action.NONE
+                  : tx.confirmations >= multisig.threshold
+                    ? Action.EXECUTE
+                    : Action.APPROVE,
+            },
+          }
+        }
 
-    makeProposeMessage: ProposalAction = (message, proposalId) => {
-      const invocation = this.executionContext.contract.populate('submit_transaction', [
-        this.contractAddress,
-        hash.getSelectorFromName(message.entrypoint),
-        message.calldata,
-        proposalId,
-      ])
-      return invocation
-    }
+        isSameProposal = (local: Call, onchain: Call): boolean => {
+          const localAddress = validateAndParseAddress(local.contractAddress)
+          const onchainAddress = validateAndParseAddress(onchain.contractAddress)
+          if (localAddress !== onchainAddress) return false
+          if (hash.getSelectorFromName(local.entrypoint) !== onchain.entrypoint) return false
+          if (!isDeepEqual(local.calldata, onchain.calldata)) return false
+          return true
+        }
 
-    makeAcceptMessage: ProposalAction = (_, proposalId) => {
-      const invocation = this.executionContext.contract.populate('confirm_transaction', [
-        proposalId,
-      ])
-      return invocation
-    }
+        makeProposeMessage: ProposalAction = (message, proposalId) => {
+          const invocation = this.executionContext.contract.populate('submit_transaction', [
+            this.contractAddress,
+            hash.getSelectorFromName(message.entrypoint),
+            message.calldata,
+            proposalId,
+          ])
+          return invocation
+        }
 
-    makeExecuteMessage: ProposalAction = (_, proposalId) => {
-      const invocation = this.executionContext.contract.populate('execute_transaction', [
-        proposalId,
-      ])
-      return invocation
-    }
+        makeAcceptMessage: ProposalAction = (_, proposalId) => {
+          const invocation = this.executionContext.contract.populate('confirm_transaction', [
+            proposalId,
+          ])
+          return invocation
+        }
 
-    makeMessage = async () => {
-      const operations = {
-        [Action.APPROVE]: this.makeAcceptMessage,
-        [Action.EXECUTE]: this.makeExecuteMessage,
-        [Action.NONE]: () => {
-          throw new Error('No action needed')
-        },
-      }
-      const message = await this.command.makeMessage()
-      if (!this.initialState.proposal) {
-        const nonce = await this.executionContext.contract.get_transactions_len()
-        return [this.makeProposeMessage(message[0], nonce)]
-      }
+        makeExecuteMessage: ProposalAction = (_, proposalId) => {
+          const invocation = this.executionContext.contract.populate('execute_transaction', [
+            proposalId,
+          ])
+          return invocation
+        }
 
-      if (!this.isSameProposal(message[0], this.initialState.proposal.data)) {
-        throw new Error('The transaction generated is different from the proposal provided')
-      }
+        makeMessage = async () => {
+          const operations = {
+            [Action.APPROVE]: this.makeAcceptMessage,
+            [Action.EXECUTE]: this.makeExecuteMessage,
+            [Action.NONE]: () => {
+              throw new Error('No action needed')
+            },
+          }
+          const message = await this.command.makeMessage()
+          if (!this.initialState.proposal) {
+            const nonce = await this.executionContext.contract.get_transactions_len()
+            return [this.makeProposeMessage(message[0], nonce)]
+          }
 
-      deps.logger.success('Generated proposal matches with the one provided')
+          if (!this.isSameProposal(message[0], this.initialState.proposal.data)) {
+            throw new Error('The transaction generated is different from the proposal provided')
+          }
 
-      return [
-        operations[this.initialState.proposal.nextAction](
-          message[0],
-          this.initialState.proposal.id,
-        ),
-      ]
-    }
+          deps.logger.success('Generated proposal matches with the one provided')
 
-    beforeExecute = async () => {
-      if (!this.initialState.proposal) deps.logger.info('About to CREATE a new multisig proposal')
-      else
-        deps.logger.info(
-          `About to ${
-            this.initialState.proposal.nextAction === Action.APPROVE ? 'APPROVE' : 'EXECUTE'
-          } the multisig proposal with ID: ${this.initialState.proposal.id}`,
-        )
-      await deps.prompt('Continue?')
-    }
+          return [
+            operations[this.initialState.proposal.nextAction](
+              message[0],
+              this.initialState.proposal.id,
+            ),
+          ]
+        }
 
-    afterExecute = async (result: Result<TransactionResponse>, proposalId?: number) => {
-      if (proposalId === undefined) deps.logger.warn('Proposal ID not found')
-      deps.logger.loading('Fetching latest multisig and proposal state...')
-      const state = await this.fetchMultisigState(this.multisigAddress, proposalId)
-      if (!state.proposal) {
-        deps.logger.error(`Multisig proposal ${proposalId} not found`)
-        return
-      }
-      const approvalsLeft = state.multisig.threshold - state.proposal.confirmations
-      const messages = {
-        [Action.EXECUTE]: `The multisig proposal reached the threshold and can be executed. Run the same command with the flag --multisigProposal=${state.proposal.id}`,
-        [Action.APPROVE]: `The multisig proposal needs ${approvalsLeft} more approvals. Run the same command with the flag --multisigProposal=${state.proposal.id}`,
-        [Action.NONE]: `The multisig proposal has been executed. No more actions needed`,
-      }
-      deps.logger.line()
-      deps.logger.info(`${messages[state.proposal.nextAction]}`)
-      deps.logger.line()
-      return { proposalId }
-    }
+        beforeExecute = async () => {
+          if (!this.initialState.proposal) deps.logger.info('About to CREATE a new multisig proposal')
+          else
+            deps.logger.info(
+              `About to ${this.initialState.proposal.nextAction === Action.APPROVE ? 'APPROVE' : 'EXECUTE'
+              } the multisig proposal with ID: ${this.initialState.proposal.id}`,
+            )
+          await deps.prompt('Continue?')
+        }
 
-    execute = async () => {
-      deps.logger.info(`Multisig State:
+        afterExecute = async (result: Result<TransactionResponse>, proposalId?: number) => {
+          if (proposalId === undefined) deps.logger.warn('Proposal ID not found')
+          deps.logger.loading('Fetching latest multisig and proposal state...')
+          const state = await this.fetchMultisigState(this.multisigAddress, proposalId)
+          if (!state.proposal) {
+            deps.logger.error(`Multisig proposal ${proposalId} not found`)
+            return
+          }
+          const approvalsLeft = state.multisig.threshold - state.proposal.confirmations
+          const messages = {
+            [Action.EXECUTE]: `The multisig proposal reached the threshold and can be executed. Run the same command with the flag --multisigProposal=${state.proposal.id}`,
+            [Action.APPROVE]: `The multisig proposal needs ${approvalsLeft} more approvals. Run the same command with the flag --multisigProposal=${state.proposal.id}`,
+            [Action.NONE]: `The multisig proposal has been executed. No more actions needed`,
+          }
+          deps.logger.line()
+          deps.logger.info(`${messages[state.proposal.nextAction]}`)
+          deps.logger.line()
+          return { proposalId }
+        }
+
+        execute = async () => {
+          deps.logger.info(`Multisig State:
         - Address: ${this.initialState.multisig.address}
         - Signers: ${this.initialState.multisig.signers}
         - Threshold: ${this.initialState.multisig.threshold}
       `)
-      if (this.initialState.proposal) {
-        deps.logger.info(`Proposal State:
+          if (this.initialState.proposal) {
+            deps.logger.info(`Proposal State:
         - ID: ${this.initialState.proposal.id}
         - Approvals: ${this.initialState.proposal.confirmations}
         - Next action: ${this.initialState.proposal.nextAction}
       `)
+          }
+          const message = await this.makeMessage()
+
+          // Underlying logger could have different style and probably a disabled prompt
+          await this.command.beforeExecute()
+          await this.beforeExecute()
+
+          deps.logger.loading(`Signing and sending transaction...`)
+          const tx = await this.provider.signAndSend(message)
+          deps.logger.loading(`Waiting for tx confirmation at ${tx.hash}...`)
+          const response = await tx.wait()
+
+          if (!response.success) {
+            deps.logger.error(`Tx was not successful: ${tx.errorMessage}`)
+          } else {
+            deps.logger.success(`Tx executed at ${tx.hash}`)
+          }
+
+          const result = {
+            responses: [
+              {
+                tx,
+                contract: tx.address,
+              },
+            ],
+          }
+
+          let proposalId = this.input.user.proposalId
+          if (!this.initialState.proposal) {
+            const txInfo = (await this.provider.provider.getTransactionReceipt(
+              tx.hash,
+            )) as InvokeTransactionReceiptResponse
+            // TODO: use contract.parseEvents?
+            proposalId = Number(num.hexToDecimalString((txInfo.events[0] as any).keys[2])) // 0 == event_id, 1 == executor, 2 == nonce/proposal_id
+          }
+
+          const data = await this.afterExecute(result, proposalId)
+
+          return data ? { ...result, data: { ...data } } : result
+        }
       }
-      const message = await this.makeMessage()
 
-      // Underlying logger could have different style and probably a disabled prompt
-      await this.command.beforeExecute()
-      await this.beforeExecute()
-
-      deps.logger.loading(`Signing and sending transaction...`)
-      const tx = await this.provider.signAndSend(message)
-      deps.logger.loading(`Waiting for tx confirmation at ${tx.hash}...`)
-      const response = await tx.wait()
-
-      if (!response.success) {
-        deps.logger.error(`Tx was not successful: ${tx.errorMessage}`)
-      } else {
-        deps.logger.success(`Tx executed at ${tx.hash}`)
-      }
-
-      const result = {
-        responses: [
-          {
-            tx,
-            contract: tx.address,
-          },
-        ],
-      }
-
-      let proposalId = this.input.user.proposalId
-      if (!this.initialState.proposal) {
-        const txInfo = (await this.provider.provider.getTransactionReceipt(
-          tx.hash,
-        )) as InvokeTransactionReceiptResponse
-        // TODO: use contract.parseEvents?
-        proposalId = Number(num.hexToDecimalString((txInfo.events[0] as any).keys[2])) // 0 == event_id, 1 == executor, 2 == nonce/proposal_id
-      }
-
-      const data = await this.afterExecute(result, proposalId)
-
-      return data ? { ...result, data: { ...data } } : result
-    }
+    return msigCommand
   }
-
-  return msigCommand
-}
