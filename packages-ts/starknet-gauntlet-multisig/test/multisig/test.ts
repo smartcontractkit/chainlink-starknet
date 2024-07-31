dba9c5811890feed17d02b3423ad3344a8f2b6e3
Augustus Chang <augustus.chang@smartcontract.com>
2024-07-31 17:33:25 -0400

    remove unnecessary file

--- packages-ts/starknet-gauntlet-multisig/test/multisig/test.ts
+++
@@ -1,9 +0,0 @@
-import { shortString } from 'starknet'
-
-const strings = shortString.splitLongString(
-  '0x0000000000004163636f756e743a20696e76616c6964207369676e6174757265',
-)
-console.log(strings)
-strings.forEach((s) => {
-  shortString.decodeShortString(s)
-})
