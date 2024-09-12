b4f2a767a4d00449bada5ecdc6bf27e22d2f0d1c
skudasov <f4hrenh9it@gmail.com>
2024-09-12 02:02:37 +0200

    update integrations-tests package from core to fit the types

--- integration-tests/smoke/ocr2_test.go
+++ integration-tests/smoke/ocr2_test.go
@@ -3,16 +3,15 @@ package smoke_test
 import (
 	"flag"
 	"fmt"
+	"github.com/smartcontractkit/chainlink/integration-tests/actions"
+	"go.uber.org/zap/zapcore"
 	"maps"
 	"os"
 	"testing"
 
-	"github.com/stretchr/testify/require"
-	"go.uber.org/zap/zapcore"
-
 	"github.com/smartcontractkit/chainlink-testing-framework/lib/logging"
-	"github.com/smartcontractkit/chainlink/integration-tests/actions"
 	"github.com/smartcontractkit/chainlink/integration-tests/docker/test_env"
+	"github.com/stretchr/testify/require"
 
 	"github.com/smartcontractkit/chainlink-starknet/integration-tests/common"
 	tc "github.com/smartcontractkit/chainlink-starknet/integration-tests/testconfig"
