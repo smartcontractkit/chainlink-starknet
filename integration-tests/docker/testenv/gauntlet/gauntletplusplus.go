6e9761800f94a8f3f9aadb2f6f932dba674acce8
chray-zhang <chray.zhang@smartcontract.com>
2024-11-12 10:36:58 -0500

    Initial commit with gpp container starting with permissions from local

---
+++ integration-tests/docker/testenv/gauntlet/gauntletplusplus.go
@@ -0,0 +1,113 @@
+package testenv
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+
+	tc "github.com/testcontainers/testcontainers-go"
+	tcwait "github.com/testcontainers/testcontainers-go/wait"
+
+	"github.com/smartcontractkit/chainlink-testing-framework/lib/docker/test_env"
+	"github.com/smartcontractkit/chainlink-testing-framework/lib/logging"
+	"github.com/smartcontractkit/chainlink-testing-framework/lib/utils/testcontext"
+
+)
+const (
+	GauntletPlusPlusPort = "7530"
+	GauntletPlusPlusInternalPort = "4444"
+)
+
+type GauntletPlusPlus struct {
+	test_env.EnvComponent
+	ExternalHTTPURL string
+	InternalHTTPURL string
+	t *testing.T
+	l zerolog.Logger
+	Image string
+}
+
+func NewGauntletPlusPlus(networks []string, image string, opts ...test_env.EnvComponentOption) *GauntletPlusPlus {
+	ms := &GauntletPlusPlus{
+		Image: image,
+		EnvComponent: test_env.EnvComponent{
+			ContainerName: "gauntlet-plus-plus",
+			Networks: 		networks,
+		},
+
+		l: log.Logger,
+	}
+
+	for _, opt := range opts {
+		opt(&ms.EnvComponent)
+	}
+	return ms
+
+}
+
+func (g *GauntletPlusPlus) WithTestLogger(t *testing.T) *GauntletPlusPlus {
+	g.l = logging.GetTestLogger(t)
+	g.t = t
+	return g
+}
+
+func (g *GauntletPlusPlus) StartContainer() error {
+	l := tc.Logger
+	if g.t != nil {
+		l = logging.CustomT{
+			T: g.t,
+			L: g.l,
+		}
+	}
+	l.Printf("Testing reached")
+	cReq, err := g.getContainerRequest()
+	if err != nil {
+		return err
+	}
+	l.Printf("Testing reached 2")
+	c, err := tc.GenericContainer(testcontext.Get(g.t), tc.GenericContainerRequest{
+		ContainerRequest: *cReq,
+		Reuse:            true,
+		Started:          true,
+		Logger:           l,
+	})
+	if err != nil {
+		return fmt.Errorf("cannot start GauntletPlusPlus container: %w", err)
+	}
+	l.Printf("Testing reached  3")
+
+	g.Container = c
+	host, err := test_env.GetHost(testcontext.Get(g.t), c)
+	if err != nil {
+		return err
+	}
+	httpPort, err := c.MappedPort(testcontext.Get(g.t), test_env.NatPort(GauntletPlusPlusPort))
+	if err != nil {
+		return err
+	}
+
+	g.ExternalHTTPURL = fmt.Sprintf("http://%s:%s", host, httpPort.Port())
+	g.InternalHTTPURL = fmt.Sprintf("http://%s:%s", g.ContainerName, GauntletPlusPlusInternalPort)
+
+	g.l.Info().
+		Any("ExternalHTTPURL", g.ExternalHTTPURL).
+		Any("ExternalHTTPURL", g.ExternalHTTPURL).
+		Str("containerName", g.ContainerName).
+		Msgf("Started Starknet container")
+
+	return nil
+}
+
+func (g *GauntletPlusPlus) getContainerRequest() (*tc.ContainerRequest, error) {
+	return &tc.ContainerRequest{
+		Name:         g.ContainerName,
+		Image:        g.Image,
+		ExposedPorts: []string{test_env.NatPortFormat(GauntletPlusPlusInternalPort)},
+		Networks:     g.Networks,
+		WaitingFor: tcwait.ForLog("Server listening at ").
+			WithStartupTimeout(30 * time.Second).
+			WithPollInterval(100 * time.Millisecond),
+	}, nil
+}
