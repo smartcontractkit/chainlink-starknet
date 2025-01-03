18c4d64bd569b1e5698bf0308aed2e4397fb5b5b
chray-zhang <chray.zhang@smartcontract.com>
2025-01-03 17:29:22 -0500

    Fixed error

--- integration-tests/common/gauntlet_plus_plus_common.go
+++ integration-tests/common/gauntlet_plus_plus_common.go
@@ -109,20 +109,6 @@ func (m *OCRv2TestState) setConfigDetailsWithGpp(ocrAddress string) error {
 	return err
 }
 
-func (m *OCRv2TestState) setConfigDetails(ocrAddress string) error {
-	cfg, err := m.LoadOCR2Config()
-	if err != nil {
-		return err
-	}
-	var parsedConfig []byte
-	parsedConfig, err = json.Marshal(cfg)
-	if err != nil {
-		return err
-	}
-	_, err = m.Clients.GauntletClient.SetConfigDetails(string(parsedConfig), ocrAddress)
-	return err
-}
-
 func (m *OCRv2TestState) DeployGauntletPP(minSubmissionValue int64, maxSubmissionValue int64, decimals int, name string, observationPaymentGjuels int64, transmissionPaymentGjuels int64) error {
 	err := m.Clients.GauntletClient.InstallDependencies()
 	if err != nil {
