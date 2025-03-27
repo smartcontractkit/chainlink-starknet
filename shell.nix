d7bca079ddf75a2471704beccc451372e3c18f2b
Dylan Tinianov <dylan.tinianov@smartcontract.com>
2025-03-27 13:17:02 -0400

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -4,15 +4,14 @@
   lib,
   scriptDir,
 }:
-with pkgs; let
+let
   go = pkgs.go_1_24;
 
-  mkShell' = mkShell.override {
-    # juno requires building with clang, not gcc
+  mkShell' = pkgs.mkShell.override {
     stdenv = pkgs.clangStdenv;
   };
 
-  golangci-lint = pkgs.buildGoModule rec {
+  custom-golangci-lint = pkgs.buildGoModule rec {
     pname = "golangci-lint";
     version = "1.62.2";
 
@@ -24,7 +23,6 @@ with pkgs; let
     };
 
     vendorHash = "sha256-SEoF+k7MYYq81v9m3eaDbIv1k9Hek5iAZ0TTJEgAsI4=";
-
     subPackages = ["cmd/golangci-lint"];
   };
 in
@@ -33,50 +31,40 @@ in
       zizmor
     ];
 
-    nativeBuildInputs =
-      [
-        stdenv.cc.cc.lib
-        (rust-bin.stable.latest.default.override {extensions = ["rust-src"];})
-        nodejs_20
-        (yarn.override {nodejs = nodejs_20;})
-        nodePackages.typescript
-        nodePackages.typescript-language-server
-        nodePackages.npm
-        python3
+    nativeBuildInputs = [
+      stdenv.cc.cc.lib
+      (pkgs.rust-bin.stable.latest.default.override { extensions = ["rust-src"]; })
+      pkgs.nodejs_20
+      (pkgs.yarn.override { nodejs = pkgs.nodejs_20; })
+      pkgs.nodePackages.typescript
+      pkgs.nodePackages.typescript-language-server
+      pkgs.nodePackages.npm
+      pkgs.python3
+      pkgs.python311Packages.ledgerwallet
+      go
+      pkgs.gopls
+      pkgs.delve
+      custom-golangci-lint
+      pkgs.gotools
+      pkgs.kubectl
+      pkgs.kubernetes-helm
+      pkgs.postgresql_15
+    ] ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
+      pkgs.libudev-zero
+      pkgs.libusb1
+    ];
 
-        python311Packages.ledgerwallet
-        go
-
-        gopls
-        delve
-        golangci-lint
-        gotools
-
-        kubectl
-        kubernetes-helm
-
-        postgresql_15 # psql
-      ]
-      ++ lib.optionals stdenv.isLinux [
-        # ledger specific packages
-        libudev-zero
-        libusb1
-      ];
-
-    LD_LIBRARY_PATH = lib.makeLibraryPath [pkgs.zlib stdenv.cc.cc.lib]; # lib64
+    LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.zlib stdenv.cc.cc.lib ];
 
     GOROOT = "${go}/share/go";
     CGO_ENABLED = 1;
     HELM_REPOSITORY_CONFIG = "${scriptDir}/.helm-repositories.yaml";
 
     shellHook = ''
-      # Update helm repositories
       helm repo update > /dev/null
-      # setup go bin for nix
       export GOBIN=$HOME/.nix-go/bin
       mkdir -p $GOBIN
       export PATH=$GOBIN:$PATH
-      # install gotestloghelper
       go install github.com/smartcontractkit/chainlink-testing-framework/tools/gotestloghelper@latest
     '';
   }
