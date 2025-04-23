98ff1fd3af2e0554d185b7d2d8393ae42054e868
chray-zhang <chray.zhang@smartcontract.com>
2025-04-23 16:39:34 -0400

    Fixed tests for ocr2

--- packages-ts/starknet-gauntlet-ocr2/src/commands/ocr2/setConfig.ts
+++ packages-ts/starknet-gauntlet-ocr2/src/commands/ocr2/setConfig.ts
@@ -187,7 +187,7 @@ const afterExecute: AfterExecute<SetConfigInput, ContractInput> = (context, inpu
     return { successfulConfiguration: false }
   }
   const events = context.contract.parseEvents(txInfo)
-  const event = events[events.length - 1]['ConfigSet']
+  const event = events[events.length - 1]['chainlink::ocr2::aggregator::Aggregator::ConfigSet']
   const offchainConfig = decodeOffchainConfigFromEventData(event.offchain_config as BigNumberish[])
 
   try {
