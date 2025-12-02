029a79c9a1b8d712903611cf0700114a9cb45e61
Eric Fornaciari <eric.fornaciari@smartcontract.com>
2025-12-02 14:47:05 -0800

    add unimplemeted relay

--- relayer/pkg/chainlink/relay.go
+++ relayer/pkg/chainlink/relay.go
@@ -19,24 +19,10 @@ import (
 var _ relaytypes.Relayer = (*relayer)(nil) //nolint:staticcheck
 
 type relayer struct {
+	relaytypes.UnimplementedRelayer
 	chain starkchain.Chain
 
 	lggr logger.Logger
-}
-
-func (r *relayer) TON() (relaytypes.TONService, error) {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (r *relayer) Solana() (relaytypes.SolanaService, error) {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (r *relayer) NewCCIPProvider(ctx context.Context, cargs relaytypes.CCIPProviderArgs) (relaytypes.CCIPProvider, error) {
-	//TODO implement me
-	panic("implement me")
 }
 
 func (r *relayer) GetChainInfo(ctx context.Context) (relaytypes.ChainInfo, error) {
