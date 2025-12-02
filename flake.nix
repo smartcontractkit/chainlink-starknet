821a43c1b1fdbe28109dab5f5ac72b88b1fde3b0
Kristijan <kristijan.rebernisak@gmail.com>
2025-12-02 16:39:25 +0100

    Fix Nix post update + format

--- flake.nix
+++ flake.nix
@@ -7,40 +7,52 @@
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-utils, rust-overlay, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; overlays = [ rust-overlay.overlays.default ]; };
-      in
-      {
-        devShell = pkgs.callPackage ./shell.nix {
-          inherit pkgs;
-          scriptDir = toString ./.; # This converts the flake's root directory to a string
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        config = {
+          permittedInsecurePackages = [
+            "python3.11-ecdsa-0.19.1"
+          ];
+        };
+        overlays = [rust-overlay.overlays.default];
+      };
+    in {
+      devShell = pkgs.callPackage ./shell.nix {
+        inherit pkgs;
+        scriptDir = toString ./.; # This converts the flake's root directory to a string
+      };
+
+      packages = {
+        starknet-devnet = pkgs.stdenv.mkDerivation rec {
+          name = "starknet-devnet";
+          src = ./ops/scripts;
+          installPhase = ''
+            mkdir -p $out/bin
+            cp $src/devnet-hardhat.sh $out/bin/${name}
+            cp $src/devnet-hardhat-down.sh $out/bin/
+            chmod +x $out/bin/${name}
+          '';
         };
 
-        packages = {
-          starknet-devnet = pkgs.stdenv.mkDerivation rec {
-            name = "starknet-devnet";
-            src = ./ops/scripts;
-            installPhase = ''
-              mkdir -p $out/bin
-              cp $src/devnet-hardhat.sh $out/bin/${name}
-              cp $src/devnet-hardhat-down.sh $out/bin/
-              chmod +x $out/bin/${name}
-            '';
-          };
-
-          starknet-devnet-down = pkgs.stdenv.mkDerivation rec {
-            name = "starknet-devnet-down";
-            src = ./ops/scripts;
-            installPhase = ''
-              mkdir -p $out/bin
-              cp $src/devnet-hardhat-down.sh $out/bin/${name}
-              chmod +x $out/bin/${name}
-            '';
-          };
+        starknet-devnet-down = pkgs.stdenv.mkDerivation rec {
+          name = "starknet-devnet-down";
+          src = ./ops/scripts;
+          installPhase = ''
+            mkdir -p $out/bin
+            cp $src/devnet-hardhat-down.sh $out/bin/${name}
+            chmod +x $out/bin/${name}
+          '';
         };
+      };
 
-        formatter = pkgs.nixpkgs-fmt;
-      });
+      formatter = pkgs.nixpkgs-fmt;
+    });
 }
